971b1354b11f4b1343eaa758426b41b328474aed
Demaro Stanberry <demaro.stanberry@gmail.com>
2020-05-21 21:46:54 -0500

    bash: (experimental) added ble.sh plugin

---
+++ bash/blerc.bash
@@ -0,0 +1,11 @@
+# set the default encoding
+bleopt input_encoding=UTF-8
+
+#define the pager to be used
+bleopt pager=less
+
+# use colors defined by LS_COLORS where applicable
+bleopt filename_ls_colors="$LS_COLORS"
+
+# define auto-suggestion text color
+ble-color-setface auto_complete fg=242
