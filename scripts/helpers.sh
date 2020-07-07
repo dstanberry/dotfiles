b97fe2c9ffb5805587d0cb507c416bc4567ae6ea
Demaro Stanberry <demaro.stanberry@gmail.com>
2020-07-07 12:39:54 -0500

    bash/zsh: don't forget to add the scripts

---
+++ scripts/helpers.sh
@@ -0,0 +1,20 @@
+# trim duplicate occurences from a string
+get_var () {
+	eval 'printf "%s\n" "${'"$1"'}"'
+}
+
+set_var () {
+	eval "$1=\"\$2\""
+}
+
+dedup_pathvar () {
+	pathvar_name="$1"
+	pathvar_value="$(get_var "$pathvar_name")"
+	deduped_path="$(perl -e 'print join(":",grep { not $seen{$_}++ } split(/:/, $ARGV[0]))' "$pathvar_value")"
+	set_var "$pathvar_name" "$deduped_path"
+}
+
+# determine if this is a wsl distribution
+is_wsl() {
+	[[ $(uname -r) == *"Microsoft"* || $(uname -r) == *"microsoft"* ]]
+}
