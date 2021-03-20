b3c97cbe557b32da94acd984413544414644f63a
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-03-20 17:36:55 -0500

    vim: add chain completion list

--- vim/lua/remote/completion/init.lua
+++ vim/lua/remote/completion/init.lua
@@ -12,3 +12,11 @@ vim.g.completion_trigger_keyword_length = 3
 
 -- set snipping parser
 vim.g.completion_enable_snippet = 'vim-vsnip'
+
+vim.g.completion_chain_complete_list = {
+  default = {
+    {complete_items = {'lsp', 'snippet'}},
+    {complete_items = {'path'}, triggered_only = {'/'}},
+    {complete_items = {'buffers'}},
+  },
+}
