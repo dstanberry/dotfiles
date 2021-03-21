88d1762ee14458dad68b9d6c7e370119069d77ed
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-03-21 15:53:08 -0500

    vim: update lua-format arguments

--- vim/lua/remote/lsp/formatters/luafmt.lua
+++ vim/lua/remote/lsp/formatters/luafmt.lua
@@ -2,6 +2,7 @@
 -- => lua formatter
 ---------------------------------------------------------------
 return {
-  formatCommand = "lua-format -i",
+  formatCommand = "lua-format -i --column-limit=80 --indent-width=2" ..
+    " --continuation-indent-width=2",
   formatStdin = true
 }
