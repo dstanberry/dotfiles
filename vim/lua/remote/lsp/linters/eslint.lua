cefecf0ab38268a0a8c8ab49c5abba93c7891247
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-03-21 18:02:01 -0500

    vim: fix syntax error with eslint command

--- vim/lua/remote/lsp/linters/eslint.lua
+++ vim/lua/remote/lsp/linters/eslint.lua
@@ -2,7 +2,7 @@
 -- => javascript linter
 ---------------------------------------------------------------
 return {
-  lintCommand = "eslint -f unix --stdin --stdin-filename ${INPUT",
+  lintCommand = "eslint -f unix --stdin --stdin-filename ${INPUT}",
   lintIgnoreExitCode = true,
   lintStdin = true,
   lintFormats = {"%f:%l:%c: %m"}
