ec9f7bed5616326ead28ef5529e322ecf30ecbca
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-04-07 21:18:25 -0500

    nvim: add support for exotic filetypes

--- vim/lua/remote/devicons/init.lua
+++ vim/lua/remote/devicons/init.lua
@@ -10,8 +10,19 @@ end
 -- define custom icons based on filetype
 devicons.setup {
   override = {
-    vimrc = {icon = "", color = "#019833", name = "vimrc"},
-    _dir_ = {icon = "", color = "#6f8fb4", name = "_dir_"}
+    ["_dir_"] = {icon = "", color = "#6f8fb4", name = "Dir"},
+    [".tmux"] = {icon = "", color = "#4d5a5e", name = "Shellscript"},
+    ["bashrc"] = {icon = "", color = "#89e051", name = "Bashrc"},
+    ["config"] = {icon = "", color = "#f14c28", name = "GitConfig"},
+    ["dircolors"] = {icon = "", color = "#4d5a5e", name = "Shellscript"},
+    ["diff-highlight"] = {icon = "", color = "#3f416a", name = "Perl"},
+    ["gitconfig"] = {icon = "", color = "#f14c28", name = "GitConfig"},
+    ["ignore"] = {icon = "", color = "#41535b", name = "GitIgnore"},
+    ["menos"] = {icon = "", color = "#cc402f", name = "Ruby"},
+    ["tmTheme"] = {icon = "謹", color = "#e37933", name = "Xml"},
+    ["vimrc"] = {icon = "", color = "#019833", name = "Vimrc"},
+    ["worktrees"] = {icon = "", color = "#41535b", name = "GitDir"},
+    ["zshrc"] = {icon = "", color = "#89e051", name = "Zshrc"}
   }
 }
 
