a4a07662f4dd2cc71ea55b96d9acb8dd21c347c9
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-04-30 15:46:02 -0500

    nvim: format prettier formatCommand

--- vim/lua/remote/lsp/formatters/prettier.lua
+++ vim/lua/remote/lsp/formatters/prettier.lua
@@ -3,8 +3,12 @@
 ---------------------------------------------------------------
 return {
   formatCommand = "prettier --stdin-filepath ${INPUT}" ..
-    " --arrow-parens always --end-of-line lf --print-width 80" ..
-    " --single-quote false --tab-width 4 --trailing-comma es5" ..
+    " --arrow-parens always" ..
+    " --end-of-line lf" ..
+    " --print-width 80" ..
+    " --single-quote false" ..
+    " --tab-width 4" ..
+    " --trailing-comma es5" ..
     " --use-tabs false",
   formatStdin = true
 }
