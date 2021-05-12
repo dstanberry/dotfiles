ada5c67b9bc6d88db19c217539e352c9a02fe2fc
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-05-12 10:24:06 -0500

    nvim: (feat) add remote tree-sitter parser

--- vim/lua/remote/tree-sitter/init.lua
+++ vim/lua/remote/tree-sitter/init.lua
@@ -2,7 +2,8 @@
 -- => Tree-Sitter Configuration
 ---------------------------------------------------------------
 -- verify tree-sitter is available
-local has_treesitter, treesitter_configs = pcall(require, 'nvim-treesitter.configs')
+local has_treesitter, treesitter_configs =
+  pcall(require, 'nvim-treesitter.configs')
 if not has_treesitter then
   return
 end
@@ -12,7 +13,9 @@ local enabled = true
 
 -- set default options
 treesitter_configs.setup {
-  ensure_installed = {'bash', 'jsonc', 'lua', 'python'},
-  highlight = {enable = enabled, use_languagetree = false, disable = {""}},
+  ensure_installed = {
+    'bash', 'c', 'c_sharp', 'cpp', 'css', 'jsonc', 'lua', 'python', 'vim'
+  },
+  highlight = {enable = enabled, use_languagetree = false, disable = {''}},
   incremental_selection = {enable = enabled}
 }
---
+++ vim/lua/remote/tree-sitter/parsers.lua
@@ -0,0 +1,17 @@
+---------------------------------------------------------------
+-- => Tree-Sitter custam parsers
+---------------------------------------------------------------
+-- verify tree-sitter is available
+local has_config, parser_config =
+  pcall(require, 'nvim-treesitter.parsers')
+if not has_config then
+  return
+end
+
+parser_config.get_parser_configs().vim = {
+  install_info = {
+    url = "https://github.com/vigoux/tree-sitter-viml",
+    files = {"src/parser.c", "src/scanner.c"}
+  },
+  used_by = {'vimrc'}
+}
