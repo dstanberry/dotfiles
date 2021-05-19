839e268da37e314116b3ca8aafc1f88b59996c84
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-05-19 10:05:15 -0500

    nvim: extend tree-sitter configuration options

--- vim/lua/remote/tree-sitter/init.lua
+++ vim/lua/remote/tree-sitter/init.lua
@@ -8,16 +8,71 @@ if not has_treesitter then
   return
 end
 
--- option to disable extension
-local enabled = true
+-- helper function to load language scheme
+local read_query = function(language, filename)
+  local front = '/vim/lua/remote/tree-sitter/queries'
+  local back = '/' .. language .. '/' .. filename .. '.scm'
+  local path = os.getenv('XDG_CONFIG_HOME') .. front .. back
+  return table.concat(vim.fn.readfile(vim.fn.expand(path)), "\n")
+end
+
+-- add local language schemes
+local languages = {vim = {'highlights', 'injections'}}
+
+for lang, list in pairs(languages) do
+  for _, scheme in ipairs(list) do
+    vim.treesitter.set_query(lang, scheme, read_query(lang, scheme))
+  end
+end
 
 -- set default options
 treesitter_configs.setup {
   ensure_installed = {
     'bash', 'c', 'c_sharp', 'comment', 'cpp', 'css', 'html', 'javascript',
     'jsdoc', 'jsonc', 'lua', -- [[ 'markdown', ]]
-    'python', 'query', 'regex', 'ruby', 'vim', 'yaml'
+    'python', 'query', 'regex', 'ruby', -- [[ 'vim', ]]
+    'yaml'
   },
-  highlight = {enable = enabled, use_languagetree = false, disable = {'vim'}},
-  incremental_selection = {enable = enabled}
+  highlight = {enable = true, use_languagetree = false, disable = {}},
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<localleader>gi',
+      node_incremental = '<localleader>gn',
+      scope_incremental = '<localleader>gs',
+      node_decremental = '<localleader>gd'
+    }
+  },
+  refactor = {
+    highlight_definitions = {enable = true},
+    highlight_current_scope = {enable = false},
+    smart_rename = {
+      enable = false,
+      keymaps = {smart_rename = '<localleader>g/'}
+    }
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      keymaps = {
+        ['<localleader>fa'] = '@function.outer',
+        ['<localleader>fi'] = '@function.inner',
+        ['<localleader>ca'] = '@conditional.outer',
+        ['<localleader>ci'] = '@conditional.inner',
+        ['<localleader>aa'] = '@parameter.outer',
+        ['<localleader>ii'] = '@parameter.inner'
+      }
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<localleader>snp"] = "@parameter.inner",
+        ["<localleader>snf"] = "@function.outer"
+      },
+      swap_previous = {
+        ["<localleader>spp"] = "@parameter.inner",
+        ["<localleader>spf"] = "@function.outer"
+      }
+    }
+  }
 }
---
+++ vim/lua/remote/tree-sitter/queries/vim/injections.scm
@@ -0,0 +1,10 @@
+(lua_statement . (_) @lua)
+(ruby_statement . (_) @ruby)
+(python_statement . (_) @python)
+(comment) @comment
+;; If we support perl at some point...
+;; (perl_statement . (_) @perl)
+
+(autocmd_statement (pattern) @regex)
+
+(comment) @comment
