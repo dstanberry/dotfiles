90dce95001a35e1f8ab5f92c5e5206cd71825b55
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-05-21 16:19:41 -0500

    nvim: fix lsp config for lua and vimscript

--- vim/lua/remote/lsp/sumneko.lua
+++ vim/lua/remote/lsp/sumneko.lua
@@ -1,18 +1,7 @@
 ---------------------------------------------------------------
--- => (sumneko) lua-language-server configuration
+-- => nlua.nvim (sumneko) lua-language-server configuration
 ---------------------------------------------------------------
--- identify project root directory
-local util = require 'lspconfig/util'
-
-local root_files = {".config", ".git"}
-
-local project_root = function(fname)
-  return util.root_pattern(unpack(root_files))(fname) or
-           util.path.dirname(fname)
-end
-
 return {
-  diagnostics = {globals = {"vim"}},
-  workspace = {library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true}},
-  root_dir = project_root
+  globals = {'vim'},
+  library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true}
 }
--- vim/lua/remote/lsp/vimls.lua
+++ vim/lua/remote/lsp/vimls.lua
@@ -4,14 +4,11 @@
 -- identify project root directory
 local util = require 'lspconfig/util'
 
-local root_files = {".config", ".git"}
+local root_files = {'.git', '.vimrc', 'init.vim', 'vimrc'}
 
 local project_root = function(fname)
   return util.root_pattern(unpack(root_files))(fname) or
            util.path.dirname(fname)
 end
 
-return {
-  vimruntime = vim.fn.expand('$VIMRUNTIME'),
-  root_dir = project_root
-}
+return {vimruntime = vim.fn.expand('$VIMRUNTIME'), root_dir = project_root}
