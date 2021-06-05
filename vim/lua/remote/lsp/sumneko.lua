a6183e886c9373e0b8b6cc6fa87150e8018b1a2a
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-06-05 09:52:57 -0500

    nvim: update root_dir for vimls and sumneko_lua

--- vim/lua/remote/lsp/sumneko.lua
+++ vim/lua/remote/lsp/sumneko.lua
@@ -1,7 +1,18 @@
 ---------------------------------------------------------------
 -- => nlua.nvim (sumneko) lua-language-server configuration
 ---------------------------------------------------------------
+local util = require('lspconfig.util')
+
+local root_files = {".vimrc", "init.lua", "init.vim", "vimrc"}
+
+-- identify project root directory
+local project_root = function(fname)
+  return util.find_git_ancestor(fname) or
+           util.root_pattern(unpack(root_files))(fname) or
+           util.path.dirname(fname)
+end
 return {
   globals = {"vim"},
+  root_dir = project_root,
   library = {[vim.fn.expand("$VIMRUNTIME/lua")] = true}
 }
--- vim/lua/remote/lsp/vimls.lua
+++ vim/lua/remote/lsp/vimls.lua
@@ -1,16 +1,15 @@
 ---------------------------------------------------------------
 -- => vim-language-server configuration
 ---------------------------------------------------------------
+local util = require('lspconfig.util')
+
+local root_files = {".vimrc", "init.vim", "vimrc"}
+
 -- identify project root directory
-local util = require('lspconfig/util')
-
-local root_files = {
-  ".git", ".vimrc", "init.vim", "vimrc"
-}
-
 local project_root = function(fname)
-  return util.root_pattern(unpack(root_files))(
-           fname) or util.path.dirname(fname)
+  return util.find_git_ancestor(fname) or
+           util.root_pattern(unpack(root_files))(fname) or
+           util.path.dirname(fname)
 end
 
 return {
