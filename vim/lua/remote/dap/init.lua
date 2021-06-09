04f9208bce02d5be12358f3e4f57bff103eec452
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-06-08 19:01:23 -0500

    nvim: add nvim-dap plugin

---
+++ vim/lua/remote/dap/init.lua
@@ -0,0 +1,44 @@
+---------------------------------------------------------------
+-- => Debug Adapter Protocol Configuration
+---------------------------------------------------------------
+-- verify dap is available
+local has_dap, dap = pcall(require, 'dap')
+if not has_dap then
+  return
+end
+
+-- enable virtual text if available
+if pcall(require, 'nvim-dap-virtual-text') then
+  vim.g.dap_virtual_text = true
+end
+
+-- debugger setup
+dap.defaults.fallback.external_terminal = {
+  command = "/usr/bin/kitty",
+  args = {"-e"}
+}
+
+dap.configurations.lua = {
+  {
+    type = "nlua",
+    request = "attach",
+    name = "Attach to running Neovim instance",
+    host = "127.0.0.1",
+    port = 54321
+  }
+}
+dap.adapters.nlua = function(callback, config)
+  callback({type = "server", host = config.host, port = config.port})
+end
+
+dap.configurations.python = {
+  {
+    type = "python",
+    request = "launch",
+    name = "Debug Current File",
+    program = "${file}",
+    args = {"--target", "api"},
+    console = "integratedTerminal"
+  }
+}
+require("dap-python").setup("python", {include_configs = true})
