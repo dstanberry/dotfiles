5e0c8ad6928b5c1397b5930c0a55e7ade9ac442a
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-08-02 19:21:20 -0500

    nvim: disable `<tab>`, `<s-tab>` pummenu nav

--- nvim/lua/remote/luasnip/keymap.lua
+++ nvim/lua/remote/luasnip/keymap.lua
@@ -20,9 +20,9 @@ end
 
 -- use tab to jump to next snippet placeholder
 local on_tab = function()
-  if vim.fn.pumvisible() == 1 then
-    return map.t "<c-n>"
-  elseif luasnip.expand_or_jumpable() then
+  -- if vim.fn.pumvisible() == 1 then
+  --   return map.t "<c-n>"
+  if luasnip.expand_or_jumpable() then
     return map.t "<plug>luasnip-expand-or-jump"
   elseif check_back_space() then
     return map.t "<tab>"
@@ -33,9 +33,9 @@ end
 
 -- use s-tab to jump to previous snippet placeholder
 local on_s_tab = function()
-  if vim.fn.pumvisible() == 1 then
-    return map.t "<c-p>"
-  elseif luasnip.jumpable(-1) then
+  -- if vim.fn.pumvisible() == 1 then
+  --   return map.t "<c-p>"
+  if luasnip.jumpable(-1) then
     return map.t "<plug>luasnip-jump-prev"
   else
     return map.t "<s-tab>"
