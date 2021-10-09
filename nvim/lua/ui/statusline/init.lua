c866e34dc627d1e4ef9e0ef4cc07aaa5767d9070
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-10-09 11:49:10 -0500

    nvim: refactor statusline

--- nvim/lua/ui/statusline/init.lua
+++ nvim/lua/ui/statusline/init.lua
@@ -2,248 +2,223 @@ local hi = require "ui.statusline.highlight"
 local data = require "ui.statusline.data"
 local lsp = require "ui.statusline.lsp"
 
-local browsers = {
-  "lir",
+local views = {
+  basic = { "help" },
+  browsers = { "lir" },
+  plugins = {
+    "Neogit",
+    "packer",
+    "qf",
+    "TelescopePrompt",
+  },
+  uri = { "man" },
 }
 
-local plugins = {
-  "Neogit",
-  "packer",
-  "qf",
-  "TelescopePrompt",
-}
+local function contains(haystack, value)
+  local found = false
+  for _, v in pairs(haystack) do
+    local match = string.match(value, v) or ""
+    if v == value or #match > 0 then
+      found = true
+      break
+    end
+  end
+  return found
+end
 
-local special = {
-  "help",
-}
-
-local uri = {
-  "man",
-}
-
-local M = {}
-
-local function add(highlight, items, conjoin)
+local function add(highlight, items, join)
   local out = ""
-  conjoin = conjoin or nil
+  join = join or nil
   for _, item in pairs(items) do
     if item ~= "" then
       if out == "" then
         out = item
       else
-        if conjoin then
-          out = string.format("%s%s", out, item)
-        else
-          out = string.format("%s %s", out, item)
-        end
+        out = join and string.format("%s%s", out, item) or string.format("%s %s", out, item)
       end
     end
   end
-  if conjoin then
-    return string.format("%s%s", highlight, out)
-  else
-    return string.format("%s%s ", highlight, out)
-  end
+  return join and string.format("%s%s", highlight, out) or string.format("%s%s ", highlight, out)
 end
 
-local function diag(bufnr, hl, prefix, count)
-  local clients = #vim.lsp.buf_get_clients(bufnr)
-  if clients == 0 or count == 0 then
-    return ""
-  end
-  return string.format("%s%s%s", hl, prefix, count)
+local function collapse_diag(hl, prefix, count)
+  return count > 0 and string.format("%s%s%s", hl, prefix, count) or ""
 end
 
-local function active(bufnr)
+local function default(state, bufnr)
   local mode = vim.fn.mode()
   local mode_hl = hi.mode(mode)
   local diagnostics = lsp.get_diagnostics(bufnr)
-  return table.concat {
-    add(mode_hl, { data.mode() }, true),
-    add(mode_hl, { data.git_branch(bufnr) }),
-    add(hi.user1, { data.relpath(bufnr) }, true),
-    add(hi.user2, { data.filename(bufnr), data.get_modified(bufnr) }),
-    hi.segment,
-    add(hi.user3, { lsp.get_messages(bufnr) }, true),
-    add(hi.custom0, {
-      diag(bufnr, hi.lsp_error, " ", diagnostics.error),
-      diag(bufnr, hi.lsp_warn, "𥉉", diagnostics.warn),
-      diag(bufnr, hi.lsp_hint, " ", diagnostics.hint),
-      diag(bufnr, hi.lsp_info, " ", diagnostics.info),
-    }),
-    add(hi.custom00, { data.get_readonly(bufnr), data.metadata(bufnr) }),
-    add(mode_hl, { data.filetype(bufnr) }),
-    add(hi.user4, { " ", data.line_number(), data.column_number() }),
-  }
+  if state == "active" then
+    return table.concat {
+      add(mode_hl, { data.mode() }),
+      add(mode_hl, { data.git_branch(bufnr) }),
+      add(hi.user1, { data.relpath(bufnr) }, true),
+      add(hi.user2, { data.filename(bufnr), data.modified(bufnr) }),
+      hi.segment,
+      add(hi.user3, { lsp.get_messages(bufnr) }, true),
+      add(hi.custom0, {
+        collapse_diag(hi.lsp_error, " ", diagnostics.error),
+        collapse_diag(hi.lsp_warn, "𥉉", diagnostics.warn),
+        collapse_diag(hi.lsp_hint, " ", diagnostics.hint),
+        collapse_diag(hi.lsp_info, " ", diagnostics.info),
+      }),
+      add(hi.custom00, { data.readonly(bufnr), data.metadata(bufnr) }),
+      add(mode_hl, { data.filetype(bufnr) }),
+      add(hi.user4, { " ", data.line_number(), data.column_number() }),
+    }
+  else
+    return table.concat {
+      add(hi.user3, { " ", data.relpath(bufnr) }, true),
+      add(hi.user3, { data.filename(bufnr), data.modified(bufnr) }),
+      hi.segment,
+    }
+  end
 end
 
-local function inactive(bufnr)
-  return table.concat {
-    add(hi.user3, { " ", data.relpath(bufnr) }, true),
-    add(hi.user3, { data.filename(bufnr), data.get_modified(bufnr) }),
-    hi.segment,
-  }
+local function explorer(state, bufnr)
+  if state == "active" then
+    local mode = vim.fn.mode()
+    local mode_hl = hi.mode(mode)
+    return table.concat {
+      add(mode_hl, { data.mode() }, true),
+      add(hi.user1, { " ", data.relpath(bufnr) }, true),
+      hi.segment,
+    }
+  else
+    return table.concat {
+      add(hi.user3, { " ", data.relpath(bufnr) }, true),
+      hi.segment,
+    }
+  end
 end
 
-local function explorer_active(bufnr)
-  local mode = vim.fn.mode()
-  local mode_hl = hi.mode(mode)
-  return table.concat {
-    add(mode_hl, { data.mode() }, true),
-    add(hi.user1, { " ", data.relpath(bufnr) }, true),
-    hi.segment,
-  }
+local function plugin(state, bufnr)
+  if state == "active" then
+    local mode = vim.fn.mode()
+    local mode_hl = hi.mode(mode)
+    return table.concat {
+      add(mode_hl, { data.mode() }, true),
+      add(mode_hl, { data.filename(bufnr) }),
+      hi.segment,
+    }
+  else
+    return table.concat {
+      add(hi.user3, { " ", data.filename(bufnr) }, true),
+      hi.segment,
+    }
+  end
 end
 
-local function explorer_inactive(bufnr)
-  return table.concat {
-    add(hi.user3, { " ", data.relpath(bufnr) }, true),
-    hi.segment,
-  }
+local function basic(state, bufnr)
+  if state == "active" then
+    local mode = vim.fn.mode()
+    local mode_hl = hi.mode(mode)
+    return table.concat {
+      add(mode_hl, { data.mode() }, true),
+      add(hi.user1, { " ", data.relpath(bufnr) }, true),
+      add(hi.user2, { data.filename(bufnr), data.modified(bufnr) }),
+      hi.segment,
+    }
+  else
+    return table.concat {
+      add(hi.user3, { " ", data.filepath(bufnr) }, true),
+      hi.segment,
+    }
+  end
 end
 
-local function plugin_active(bufnr)
-  local mode = vim.fn.mode()
-  local mode_hl = hi.mode(mode)
-  return table.concat {
-    add(mode_hl, { data.mode() }, true),
-    add(mode_hl, { data.filename(bufnr) }),
-    hi.segment,
-  }
+local function uri(state, bufnr)
+  if state == "active" then
+    local mode = vim.fn.mode()
+    local mode_hl = hi.mode(mode)
+    return table.concat {
+      add(mode_hl, { data.mode() }, true),
+      add(hi.user1, { " ", data.relpath(bufnr), "/" }, true),
+      add(hi.user2, { data.filename(bufnr), data.modified(bufnr) }),
+      hi.segment,
+    }
+  else
+    return table.concat {
+      add(hi.user3, { " ", data.filepath(bufnr) }, true),
+      hi.segment,
+    }
+  end
 end
 
-local function plugin_inactive(bufnr)
-  return table.concat {
-    add(hi.user3, { " ", data.filename(bufnr) }, true),
-    hi.segment,
-  }
+local function simple(state, bufnr)
+  if state == "active" then
+    local mode = vim.fn.mode()
+    local mode_hl = hi.mode(mode)
+    return table.concat {
+      add(mode_hl, { data.mode() }, true),
+      add(mode_hl, { data.git_branch(bufnr) }),
+      add(hi.user2, { data.filename(bufnr), data.modified(bufnr) }),
+      hi.segment,
+      add(hi.custom00, { data.readonly(bufnr), data.metadata(bufnr) }),
+      add(mode_hl, { data.filetype(bufnr) }),
+      add(hi.user4, { " ", data.line_number(), data.column_number() }),
+    }
+  else
+    return hi.segment
+  end
 end
 
-local function special_active(bufnr)
-  local mode = vim.fn.mode()
-  local mode_hl = hi.mode(mode)
-  return table.concat {
-    add(mode_hl, { data.mode() }, true),
-    add(hi.user1, { " ", data.relpath(bufnr) }, true),
-    add(hi.user2, { data.filename(bufnr), data.get_modified(bufnr) }),
-    hi.segment,
-  }
-end
-
-local function special_inactive(bufnr)
-  return table.concat {
-    add(hi.user3, { " ", data.filepath(bufnr) }, true),
-    hi.segment,
-  }
-end
-
-local function uri_active(bufnr)
-  local mode = vim.fn.mode()
-  local mode_hl = hi.mode(mode)
-  return table.concat {
-    add(mode_hl, { data.mode() }, true),
-    add(hi.user1, { " ", data.relpath(bufnr), "/" }, true),
-    add(hi.user2, { data.filename(bufnr), data.get_modified(bufnr) }),
-    hi.segment,
-  }
-end
-
-local function uri_inactive(bufnr)
-  return table.concat {
-    add(hi.user3, { " ", data.filepath(bufnr) }, true),
-    hi.segment,
-  }
-end
-
-local function simple_active(bufnr)
-  local mode = vim.fn.mode()
-  local mode_hl = hi.mode(mode)
-  return table.concat {
-    add(mode_hl, { data.mode() }, true),
-    add(mode_hl, { data.git_branch(bufnr) }),
-    add(hi.user2, { data.filename(bufnr), data.get_modified(bufnr) }),
-    hi.segment,
-    add(hi.custom00, { data.get_readonly(bufnr), data.metadata(bufnr) }),
-    add(mode_hl, { data.filetype(bufnr) }),
-    add(hi.user4, { " ", data.line_number(), data.column_number() }),
-  }
-end
-
-local function simple_inactive()
-  return hi.segment
-end
+local M = {}
 
 M.focus = function(win_id)
   if not vim.api.nvim_win_is_valid(win_id) then
-    return simple_inactive()
+    return simple("inactive", _)
   end
+  local state = "active"
   local bufnr = vim.api.nvim_win_get_buf(win_id)
   local type = vim.api.nvim_buf_get_option(bufnr, "filetype")
-  for _, t in ipairs(browsers) do
-    if type == t then
-      return explorer_active(bufnr)
-    end
+  if contains(views.browsers, type) then
+    return explorer(state, bufnr)
   end
-  for _, t in ipairs(plugins) do
-    local match = string.match(type, t) or ""
-    if type == t or #match > 0 then
-      return plugin_active(bufnr)
-    end
+  if contains(views.plugins, type) then
+    return plugin(state, bufnr)
   end
-  for _, t in ipairs(special) do
-    if type == t then
-      return special_active(bufnr)
-    end
+  if contains(views.basic, type) then
+    return basic(state, bufnr)
   end
-  for _, t in ipairs(uri) do
-    if type == t then
-      return uri_active(bufnr)
-    end
+  if contains(views.uri, type) then
+    return uri(state, bufnr)
   end
   type = vim.fn.getftype(data.filepath(bufnr))
   local name = vim.fn.bufname(bufnr)
-  local line = ""
   if type == "file" or #name > 0 then
-    line = active(bufnr)
+    return default(state, bufnr)
   else
-    line = simple_active(bufnr)
+    return simple(state, bufnr)
   end
-  return line
 end
 
 M.dim = function(win_id)
+  local state = "inactive"
   if not vim.api.nvim_win_is_valid(win_id) then
-    return simple_inactive()
+    return simple(state, _)
   end
   local bufnr = vim.api.nvim_win_get_buf(win_id)
   local type = vim.api.nvim_buf_get_option(bufnr, "filetype")
   local name = vim.fn.bufname(bufnr)
-  for _, t in ipairs(browsers) do
-    if type == t then
-      return explorer_inactive(bufnr)
-    end
+  if contains(views.browsers, type) then
+    return explorer(state, bufnr)
   end
-  for _, t in ipairs(plugins) do
-    local match = string.match(type, t) or ""
-    if type == t or #match > 0 then
-      return plugin_inactive(bufnr)
-    end
+  if contains(views.plugins, type) then
+    return plugin(state, bufnr)
   end
-  for _, t in ipairs(special) do
-    if type == t then
-      return special_inactive(bufnr)
-    end
+  if contains(views.basic, type) then
+    return basic(state, bufnr)
   end
-  for _, t in ipairs(uri) do
-    if type == t then
-      return uri_inactive(bufnr)
-    end
+  if contains(views.uri, type) then
+    return uri(state, bufnr)
   end
   type = vim.fn.getftype(data.filepath(bufnr))
   if type == "file" or #name > 0 then
-    return inactive(bufnr)
+    return default(state, bufnr)
   else
-    return simple_inactive()
+    return simple(state, bufnr)
   end
 end
 
