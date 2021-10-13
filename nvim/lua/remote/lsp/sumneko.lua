ffeeb6e8ed665bf76401424a04d240ea618c1460
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-10-12 21:40:41 -0500

    nvim: update lua-dev (sumneko_lua) configuration

--- nvim/lua/remote/lsp/sumneko.lua
+++ nvim/lua/remote/lsp/sumneko.lua
@@ -4,20 +4,16 @@ if not ok then
   return {}
 end
 
--- determine operating system
 local system_name
 if vim.fn.has "mac" == 1 then
   system_name = "macOS"
 elseif vim.fn.has "unix" == 1 then
   system_name = "Linux"
-  -- elseif vim.fn.has "win32" == 1 then
-  --   system_name = "Windows"
 else
   print "Unsupported system for sumneko"
   return
 end
 
--- resolve the location of lua-language-server binary
 local sumneko_root_path = string.format("%s/lspconfig/sumneko_lua/lua-language-server", vim.fn.stdpath "cache")
 local sumneko_binary = string.format("%s/bin/%s/lua-language-server", sumneko_root_path, system_name)
 
@@ -26,6 +22,18 @@ local M = {}
 M.config = luadev.setup {
   lspconfig = {
     cmd = { sumneko_binary, "-E", string.format("%s/main.lua", sumneko_root_path) },
+    settings = {
+      Lua = {
+        completion = {
+          showWord = "Disable",
+          -- keywordSnippet = "Disable",
+        },
+        diagnostics = {
+          enable = true,
+          globals = { "dump", "profile", "reload" },
+        },
+      },
+    },
   },
 }
 
