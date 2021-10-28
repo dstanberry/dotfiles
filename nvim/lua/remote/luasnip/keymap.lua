1ec4eaf9cc392403c1e5962419acc118b08380d7
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-10-27 20:44:35 -0500

    nvim: fix tab keymaps for luasnip

--- nvim/lua/remote/luasnip/keymap.lua
+++ nvim/lua/remote/luasnip/keymap.lua
@@ -6,29 +6,18 @@ end
 
 local map = require "util.map"
 
-local check_back_space = function()
-  local col = vim.fn.col "." - 1
-  if col == 0 or vim.fn.getline("."):sub(col, col):match "%s" then
-    return true
-  else
-    return false
-  end
-end
-
 local on_tab = function()
   if luasnip.expand_or_jumpable() then
-    return luasnip.expand_or_jump()
-  elseif check_back_space() then
-    return map.t "<tab>"
+    return map.t "<plug>luasnip-expand-or-jump"
   end
+  return map.t "<tab>"
 end
 
 local on_s_tab = function()
   if luasnip.jumpable(-1) then
-    return luasnip.jump(-1)
-  else
-    return map.t "<s-tab>"
+    return map.t "<plug>luasnip-jump-prev"
   end
+  return map.t "<s-tab>"
 end
 
 local function next_choice()
@@ -39,10 +28,10 @@ local function prev_choice()
   return luasnip.change_choice(-1)
 end
 
-map.imap("<tab>", on_tab)
-map.smap("<tab>", on_tab)
-map.imap("<s-tab>", on_s_tab)
-map.smap("<s-tab>", on_s_tab)
+map.imap("<tab>", on_tab, { expr = true })
+map.smap("<tab>", on_tab, { expr = true })
+map.imap("<s-tab>", on_s_tab, { expr = true })
+map.smap("<s-tab>", on_s_tab, { expr = true })
 map.imap("<c-d>", next_choice)
 map.smap("<c-d>", next_choice)
 map.imap("<c-f>", prev_choice)
