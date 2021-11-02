b53a42a1c42593956a6719156ea5a7f4a3e96e01
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-11-02 12:00:10 -0500

    nvim: update treesitter query for lua

--- nvim/after/queries/lua/injections.scm
+++ nvim/after/queries/lua/injections.scm
@@ -9,6 +9,19 @@
 
 	(#eq? @_vimscript_identifier_1 "vim")
 	(#eq? @_vimscript_identifier_2 "cmd")
-	(#match? @vim "^\\[\\[")
+	(#match? @vim "^[\"']")
+)
 
+(
+	(function_call
+		(field_expression
+			(identifier) @_vimscript_identifier_1
+			(property_identifier) @_vimscript_identifier_2)
+		(arguments
+			(string) @vim)
+	)
+
+	(#eq? @_vimscript_identifier_1 "vim")
+	(#eq? @_vimscript_identifier_2 "cmd")
+	(#match? @vim "^\\[\\[")
 )
