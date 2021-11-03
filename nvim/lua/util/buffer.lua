e0d1e772d57369fb1b405cda8c080c50bbe80530
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-11-03 12:30:31 -0500

    nvim: replace `vim.fn.execute()` with `vim.cmd()`

--- nvim/lua/util/buffer.lua
+++ nvim/lua/util/buffer.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.create_scratch_buffer()
   local ft = vim.fn.input "scratch buffer filetype: "
-  vim.fn.execute "20new [Scratch]"
+  vim.cmd "20new [Scratch]"
   vim.bo.bufhidden = "wipe"
   vim.bo.buflisted = false
   vim.bo.buftype = "nofile"
@@ -10,12 +10,13 @@ function M.create_scratch_buffer()
   if ft then
     vim.bo.filetype = ft
   end
+  return
 end
 
 function M.create_md_note()
   local dir = vim.env.hash_n or vim.env.HOME
   local fname = ("%s/%s.md"):format(dir, os.date "%m_%d_%y")
-  vim.fn.execute(("edit %s"):format(fname))
+  vim.cmd(("edit %s"):format(fname))
   local bufnr = vim.fn.bufnr(vim.fn.expand(("%s"):format(fname), true))
   for _, win_id in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
     local open_bufnr = vim.api.nvim_win_get_buf(win_id)
