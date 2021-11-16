98e579e14c44d17812009d1d113bd1bb4ee1d814
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-11-16 14:41:08 -0600

    nvim: use function_node in luasnip configuration

--- nvim/lua/remote/luasnip/snippets/lua.lua
+++ nvim/lua/remote/luasnip/snippets/lua.lua
@@ -4,91 +4,106 @@ if not ok then
   return
 end
 
+local util = require "remote.luasnip.util"
+
 local s = luasnip.snippet
-local choice = luasnip.choice_node
-local ins = luasnip.insert_node
-local snip = luasnip.snippet_node
-local txt = luasnip.text_node
+local c = luasnip.choice_node
+local i = luasnip.insert_node
+local sn = luasnip.snippet_node
+local t = luasnip.text_node
 
 local M = {}
 
 M.config = {
   lua = {
     s({ trig = "[[-", wordTrig = false, hidden = true }, {
-      txt "--[[",
-      txt { "", "\t" },
-      ins(0),
-      txt { "", "--]]" },
+      t "--[[",
+      t { "", "\t" },
+      i(0),
+      t { "", "--]]" },
     }),
     s({ trig = "[[;", wordTrig = false, hidden = true }, {
-      txt "[[",
-      txt { "", "\t" },
-      ins(0),
-      txt { "", "]]" },
+      t "[[",
+      t { "", "\t" },
+      i(0),
+      t { "", "]]" },
     }),
     s({ trig = "ig", wordTrig = true, hidden = true }, {
-      txt "-- stylua: ignore",
+      t "-- stylua: ignore",
     }),
     s({ trig = "fn", wordTrig = true, dscr = "function(param) .. end" }, {
-      txt "function(",
-      ins(1, "_"),
-      txt { ")", "\t" },
-      ins(0),
-      txt { "", "end" },
+      t "function(",
+      i(1, "_"),
+      t { ")", "\t" },
+      i(0),
+      t { "", "end" },
     }),
     s({ trig = "lf", wordTrig = true, dscr = "local function f(param) .. end" }, {
-      txt "local function ",
-      ins(1, "f"),
-      txt "(",
-      ins(2, "_"),
-      txt ")",
-      txt { "", "\t" },
-      ins(0),
-      txt { "", "end" },
+      t "local function ",
+      i(1, "f"),
+      t "(",
+      i(2, "_"),
+      t ")",
+      t { "", "\t" },
+      i(0),
+      t { "", "end" },
     }),
     s({ trig = "tf", wordTrig = true, dscr = "local f = function(param) .. end" }, {
-      txt "local ",
-      ins(1, "f"),
-      txt " = function(",
-      ins(2, "_"),
-      txt ")",
-      txt { "", "\t" },
-      ins(0),
-      txt { "", "end" },
+      t "local ",
+      i(1, "f"),
+      t " = function(",
+      i(2, "_"),
+      t ")",
+      t { "", "\t" },
+      i(0),
+      t { "", "end" },
     }),
     s({ trig = "if", wordTrig = true }, {
-      txt "if ",
-      ins(1),
-      txt { " then", "\t" },
-      ins(0),
-      txt { "", "end" },
+      t "if ",
+      i(1),
+      t { " then", "\t" },
+      i(0),
+      t { "", "end" },
     }),
     s({ trig = "lt", wordTrig = true, dscr = "local var = { .. }" }, {
-      txt "local ",
-      ins(1, "var"),
-      txt " = {",
-      txt { "", "\t" },
-      ins(0),
-      txt { "", "}" },
+      t "local ",
+      i(1, "var"),
+      t " = {",
+      t { "", "\t" },
+      i(0),
+      t { "", "}" },
     }),
     s("for", {
-      txt "for ",
-      choice(1, {
-        snip(nil, {
-          ins(1, "k"),
-          txt ", ",
-          ins(2, "v"),
-          txt " in ",
-          choice(3, { txt "pairs", txt "ipairs" }),
-          txt "(",
-          ins(4),
-          txt ")",
+      t "for ",
+      c(1, {
+        sn(nil, {
+          i(1, "k"),
+          t ", ",
+          i(2, "v"),
+          t " in ",
+          c(3, { t "pairs", t "ipairs" }),
+          t "(",
+          i(4),
+          t ")",
         }),
-        snip(nil, { ins(1, "i"), txt " = ", ins(2), txt ", ", ins(3) }),
+        sn(nil, { i(1, "i"), t " = ", i(2), t ", ", i(3) }),
       }),
-      txt { " do", "\t" },
-      ins(0),
-      txt { "", "end" },
+      t { " do", "\t" },
+      i(0),
+      t { "", "end" },
+    }),
+    s({ trig = "ok", wordTrig = true }, {
+      t "local ",
+      i(1, "ok"),
+      t ", ",
+      i(2, "mod"),
+      t [[ = pcall(require, "]],
+      util.same(2),
+      t [[")]],
+      t { "", "if not " },
+      util.same(1),
+      t { " then", "\treturn" },
+      t { "", "end" },
     }),
   },
 }
--- nvim/lua/remote/luasnip/util.lua
+++ nvim/lua/remote/luasnip/util.lua
@@ -7,21 +7,9 @@ end
 local M = {}
 
 local s = luasnip.snippet
+local f = luasnip.function_node
 local i = luasnip.insert_node
 local t = luasnip.text_node
-
-M.char_count_same = function(c1, c2)
-  local line = vim.api.nvim_get_current_line()
-  local _, ct1 = string.gsub(line, "%" .. c1, "")
-  local _, ct2 = string.gsub(line, "%" .. c2, "")
-  return ct1 == ct2
-end
-
-M.even_count = function(char)
-  local line = vim.api.nvim_get_current_line()
-  local _, ct = string.gsub(line, char, "")
-  return ct % 2 == 0
-end
 
 M.autopair = function(pair_begin, pair_end, ...)
   local function negate(fn, ...)
@@ -38,6 +26,25 @@ M.autopair = function(pair_begin, pair_end, ...)
     { t { pair_begin }, i(1), t { pair_end } },
     { condition = part(negate, part(..., pair_begin, pair_end)) }
   )
+end
+
+M.char_count_same = function(c1, c2)
+  local line = vim.api.nvim_get_current_line()
+  local _, ct1 = string.gsub(line, "%" .. c1, "")
+  local _, ct2 = string.gsub(line, "%" .. c2, "")
+  return ct1 == ct2
+end
+
+M.even_count = function(char)
+  local line = vim.api.nvim_get_current_line()
+  local _, ct = string.gsub(line, char, "")
+  return ct % 2 == 0
+end
+
+M.same = function(index)
+  return f(function(args)
+    return args[1]
+  end, { index })
 end
 
 return M
