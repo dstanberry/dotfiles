dfa4748ff3ffec7554d7bf0be5b91d9cccf3aac9
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-11-23 10:54:27 -0600

    nvim: add utility to install lua-language-server

--- nvim/lua/remote/lsp/servers/sumneko_lua.lua
+++ nvim/lua/remote/lsp/servers/sumneko_lua.lua
@@ -4,6 +4,8 @@ if not ok then
   return
 end
 
+local util = require "util"
+
 local fname
 local system_name
 if vim.fn.has "mac" == 1 then
@@ -20,14 +22,28 @@ else
   return
 end
 
-local sumneko_root_path = string.format("%s/lspconfig/lua-language-server", vim.fn.stdpath "data")
-local sumneko_binary = string.format("%s/bin/%s/%s", sumneko_root_path, system_name, fname)
+local basedir = string.format("%s/lspconfig", vim.fn.stdpath "data")
+local root_path = string.format("%s/lua-language-server", basedir)
+local executable = string.format("%s/bin/%s/%s", root_path, system_name, fname)
 
 local M = {}
 
+M.setup = function(force)
+  local install_cmd = [[
+    git clone https://github.com/sumneko/lua-language-server
+    cd lua-language-server
+    git submodule update --init --recursive
+    cd 3rd/luamake
+    ./compile/install.sh
+    cd ../..
+    ./3rd/luamake/luamake rebuild
+  ]]
+  util.terminal.install_package("lua-language-server", basedir, basedir, install_cmd, force)
+end
+
 M.config = luadev.setup {
   lspconfig = {
-    cmd = { sumneko_binary, "-E", string.format("%s/main.lua", sumneko_root_path) },
+    cmd = { executable, "-E", string.format("%s/main.lua", root_path) },
     settings = {
       Lua = {
         completion = {
