efd0c7e6241ef7633de33e70ffa349067578af30
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-11-29 13:21:45 -0600

    nvim: revert luasnip keymap for expanding snippets

--- nvim/lua/remote/luasnip/keymap.lua
+++ nvim/lua/remote/luasnip/keymap.lua
@@ -5,14 +5,14 @@ local expand_or_jump = function()
   if luasnip.expand_or_jumpable() then
     return map.t "<plug>luasnip-expand-or-jump"
   end
-  return map.t "<c-right>"
+  return map.t "<tab>"
 end
 
 local jump_prev = function()
   if luasnip.jumpable(-1) then
     return map.t "<plug>luasnip-jump-prev"
   end
-  return map.t "<c-left>"
+  return map.t "<s-tab>"
 end
 
 local function next_choice()
@@ -23,10 +23,10 @@ local function prev_choice()
   return luasnip.change_choice(-1)
 end
 
-map.imap("<c-right>", expand_or_jump, { expr = true })
-map.smap("<c-right>", expand_or_jump, { expr = true })
-map.imap("<c-left>", jump_prev, { expr = true })
-map.smap("<c-left>", jump_prev, { expr = true })
+map.imap("<tab>", expand_or_jump, { expr = true })
+map.smap("<tab>", expand_or_jump, { expr = true })
+map.imap("<s-tab>", jump_prev, { expr = true })
+map.smap("<s-tab>", jump_prev, { expr = true })
 map.imap("<c-d>", next_choice)
 map.smap("<c-d>", next_choice)
 map.imap("<c-f>", prev_choice)
