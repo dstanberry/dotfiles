aa0bbf1fd9925c2196e62b7d08829897df77f1d3
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-12-04 14:31:15 -0600

    nvim: add implementation for `vim.ui` interface

---
+++ nvim/lua/util/window.lua
@@ -0,0 +1,85 @@
+local util = require "util"
+
+local nnoremap = util.map.nnoremap
+local inoremap = util.map.inoremap
+
+local M = {}
+
+M.border_line = "─"
+
+M.calculate_width = function(lines)
+  local max_width = math.ceil(vim.o.columns * 0.8)
+  local max_length = 0
+  for _, line in pairs(lines) do
+    if #line > max_length then
+      max_length = #line
+    end
+  end
+  return max_length <= max_width and max_length or max_width
+end
+
+local set_cursor = function()
+  local current_line = vim.fn.line "."
+  local max_lines = vim.api.nvim_buf_line_count(0)
+  if current_line < 3 and max_lines >= 3 then
+    vim.api.nvim_win_set_cursor(0, { 3, 1 })
+  end
+end
+
+M.popup_create = function(opts)
+  local lines, syntax = opts.lines or {}, opts.syntax
+  opts.border = opts.border or "rounded"
+  local popup_bufnr, winnr = vim.lsp.util.open_floating_preview(lines, syntax, opts)
+  -- vim.api.nvim_win_set_option(winnr, "winhl", "Normal:Normal")
+  print(vim.inspect(lines))
+  if opts.enter then
+    vim.api.nvim_set_current_win(winnr)
+    inoremap("jk", function()
+      vim.cmd [[stopinsert]]
+      vim.api.nvim_win_close(0, true)
+    end, { buffer = popup_bufnr })
+    nnoremap("<esc>", function()
+      vim.api.nvim_win_close(0, true)
+    end, { buffer = popup_bufnr })
+    nnoremap("q", function()
+      vim.api.nvim_win_close(0, true)
+    end, { buffer = popup_bufnr })
+  end
+  if opts.on_confirm then
+    inoremap("<cr>", function()
+      opts.on_confirm()
+      vim.cmd [[stopinsert]]
+    end, { buffer = popup_bufnr })
+    nnoremap("<cr>", function()
+      opts.on_confirm()
+    end, { buffer = popup_bufnr })
+  end
+  if opts.input then
+    vim.cmd "startinsert"
+    vim.api.nvim_buf_set_option(popup_bufnr, "modifiable", true)
+  end
+  if opts.prompt and opts.prompt.enable then
+    vim.api.nvim_buf_set_option(popup_bufnr, "buftype", "prompt")
+    vim.fn.prompt_setprompt(popup_bufnr, opts.prompt.prefix)
+    vim.api.nvim_buf_add_highlight(popup_bufnr, -1, opts.prompt.highlight, #lines, 0, #opts.prompt.prefix)
+    vim.api.nvim_buf_set_option(popup_bufnr, "ft", "UIPrompt")
+  end
+  if opts.set_cursor then
+    vim.api.nvim_win_set_cursor(winnr, { 3, 1 })
+    util.define_augroup {
+      name = "update_cursor",
+      clear = true,
+      autocmds = {
+        {
+          event = "CursorMoved",
+          callback = function()
+            set_cursor()
+          end,
+        },
+      },
+    }
+  end
+  return popup_bufnr, winnr
+end
+
+return M
---
+++ nvim/plugin/input.lua
@@ -0,0 +1,92 @@
+-- verify vim.ui interface is available
+if not vim.ui then
+  return
+end
+
+local ui = require "util.window"
+local nnoremap = require("util.map").nnoremap
+
+local options = {
+  prefix = " ❯ ",
+}
+
+local confirm = function(items, on_choice, key)
+  local choice = tonumber(vim.fn.expand "<cword>")
+  if key then
+    choice = key
+  end
+
+  vim.api.nvim_win_close(0, true)
+  on_choice(items[choice], choice)
+end
+
+vim.ui.input = function(opts, on_confirm)
+  vim.validate {
+    on_confirm = { on_confirm, "function", false },
+  }
+  opts = opts or {}
+
+  local lines = {}
+  local title = opts.prompt
+  lines = { title, string.rep(ui.border_line, 30), unpack(lines) }
+
+  local popup_bufnr, _ = ui.popup_create {
+    width = 30,
+    lines = lines,
+    height = 3,
+    enter = true,
+    input = true,
+    prompt = {
+      enable = true,
+      prefix = options.prefix,
+      highlight = "Float",
+    },
+    on_confirm = function()
+      local input = vim.trim(vim.fn.getline("."):sub(#options.prefix + 1, -1))
+      vim.api.nvim_win_close(0, true)
+      on_confirm(input)
+    end,
+  }
+  vim.api.nvim_buf_add_highlight(popup_bufnr, -1, "Title", 0, 0, #title)
+  vim.api.nvim_buf_add_highlight(popup_bufnr, -1, "FloatBorder", 1, 0, -1)
+end
+
+vim.ui.select = function(items, opts, on_choice)
+  vim.validate {
+    items = { items, "table", false },
+    on_choice = { on_choice, "function", false },
+  }
+  opts = opts or {}
+  local choices = { opts.prompt or "Select one of:" }
+  local format_item = opts.format_item or tostring
+  for i, item in pairs(items) do
+    table.insert(choices, string.format("[%d] %s", i, format_item(item)))
+  end
+
+  local title = table.remove(choices, 1)
+  local width = ui.calculate_width(choices)
+  choices = { title, string.rep(ui.border_line, width), unpack(choices) }
+
+  local popup_bufnr, _ = ui.popup_create {
+    lines = choices,
+    enter = true,
+    set_cursor = true,
+    on_confirm = function()
+      confirm(items, on_choice)
+    end,
+  }
+  vim.api.nvim_buf_add_highlight(popup_bufnr, -1, "Title", 0, 0, #title)
+  vim.api.nvim_buf_add_highlight(popup_bufnr, -1, "FloatBorder", 1, 0, -1)
+
+  for k, _ in ipairs(choices) do
+    if k > 2 then
+      nnoremap {
+        string.format("%d", k - 2),
+        function()
+          confirm(items, on_choice, k - 2)
+        end,
+        buffer = true,
+      }
+    end
+  end
+end
