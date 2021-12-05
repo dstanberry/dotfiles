611359e2cde4cd0655562c71c2a24bc34cc10a65
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-12-05 15:34:35 -0600

    nvim: update `dump()` debugging function

--- nvim/lua/util/globals.lua
+++ nvim/lua/util/globals.lua
@@ -1,5 +1,25 @@
 _G.dump = function(...)
-  print(vim.inspect(...))
+  local objects = {}
+  for i = 1, select("#", ...) do
+    local v = select(i, ...)
+    table.insert(objects, vim.inspect(v))
+  end
+
+  print(table.concat(objects, "\n"))
+  return ...
+end
+
+_G.dump_text = function(...)
+  local objects = {}
+  for i = 1, select("#", ...) do
+    local v = select(i, ...)
+    table.insert(objects, vim.inspect(v))
+  end
+
+  local lines = vim.split(table.concat(objects, "\n"), "\n")
+  local lnum = vim.api.nvim_win_get_cursor(0)[1]
+  vim.fn.append(lnum, lines)
+  return ...
 end
 
 _G.fold_text = function()
