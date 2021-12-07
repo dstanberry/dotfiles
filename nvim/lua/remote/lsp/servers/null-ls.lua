4d66795a60bb87e40ca4b6b6a02b3a86d1b7ab15
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-12-07 17:06:48 -0600

    nvim: update null-ls configuration

--- nvim/lua/remote/lsp/servers/null-ls.lua
+++ nvim/lua/remote/lsp/servers/null-ls.lua
@@ -6,25 +6,33 @@ end
 
 local M = {}
 
+local sources = {
+  null_ls.builtins.code_actions.gitsigns,
+  null_ls.builtins.diagnostics.flake8,
+  null_ls.builtins.diagnostics.markdownlint,
+  null_ls.builtins.diagnostics.shellcheck.with {
+    diagnostics_format = "[#{c}] (#{s}) #{m}",
+  },
+  null_ls.builtins.diagnostics.vint,
+  null_ls.builtins.formatting.eslint_d,
+  null_ls.builtins.formatting.isort,
+  null_ls.builtins.formatting.prettier.with {
+    extra_args = function(params)
+      return params.ft == "markdown" and { "$FILENAME", "--print-width", "80", "--prose-wrap", "always" }
+        or { "$FILENAME" }
+    end,
+  },
+  null_ls.builtins.formatting.shfmt.with { args = { "-i", "2", "-ci", "-sr", "-s", "-bn" } },
+  null_ls.builtins.formatting.stylua,
+  null_ls.builtins.formatting.yapf,
+}
+
 M.config = null_ls.config {
   debug = false,
   debounce = 150,
   diagnostics_format = "(#{s}) #{m}",
   save_after_format = false,
-  sources = {
-    null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.diagnostics.flake8,
-    null_ls.builtins.diagnostics.markdownlint,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.diagnostics.vint,
-    null_ls.builtins.formatting.eslint_d,
-    null_ls.builtins.formatting.isort,
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.shfmt.with { args = { "-i", "2", "-ci", "-sr", "-s", "-bn" } },
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.yapf,
-  },
+  sources = sources,
 }
 
 return M
