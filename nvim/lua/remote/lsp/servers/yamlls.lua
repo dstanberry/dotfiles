dc2fef5693782b8f38e2819ffdec017c8fd5d3ce
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-12-06 21:24:17 -0600

    nvim: add yamlls configuration

---
+++ nvim/lua/remote/lsp/servers/yamlls.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.config = {
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
+      },
+    },
+  },
+}
+
+return M
