b4bcbb40c1b8698d542e140431585f8e108a3a18
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-12-08 20:36:32 -0600

    nvim: (chore) sort commands list

--- nvim/plugin/commands.lua
+++ nvim/plugin/commands.lua
@@ -1,11 +1,11 @@
 local util = require "util"
 
 util.define_command {
-  name = "W",
-  callback = util.buffer.sudo_write,
+  name = "S",
+  callback = util.buffer.create_scratch,
 }
 
 util.define_command {
-  name = "S",
-  callback = util.buffer.create_scratch,
+  name = "W",
+  callback = util.buffer.sudo_write,
 }
