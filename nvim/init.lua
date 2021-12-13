619d3cbc36e26389c701eea898e7a0f0ab759e8d
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-12-13 16:59:50 -0600

    nvim: (chore) code cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,24 +25,24 @@ vim.g.markdown_fenced_languages = {
 }
 
 vim.g.did_load_filetype = 1
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
-vim.g.loaded_netrwFileHandlers = 1
-vim.g.loaded_gzip = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
+vim.g.loaded_2html_plugin = 1
 vim.g.loaded_getscript = 1
 vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_2html_plugin = 1
+vim.g.loaded_gzip = 1
 vim.g.loaded_logipat = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
 vim.g.loaded_rrhelper = 1
 vim.g.loaded_spellfile_plugin = 1
-vim.g.loaded_matchit = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
 
 local cache = vim.fn.stdpath "cache"
 local backup = string.format("%s/backup", cache)
