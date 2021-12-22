2f35e43074a2e6353791f50a51da72c79910b895
Demaro Stanberry <demaro.stanberry@gmail.com>
2021-12-22 12:45:40 -0600

    nvim: change highlight color for Whitespace

--- nvim/lua/ui/theme/groups.lua
+++ nvim/lua/ui/theme/groups.lua
@@ -70,7 +70,7 @@ M.apply = function(c)
   hi.Conceal = { guifg = c.base0D, guibg = c.base00, gui = nil, guisp = nil }
   hi.Cursor = { guifg = c.base00, guibg = c.base05, gui = nil, guisp = nil }
   hi.NonText = { guifg = c.base11, guibg = nil, gui = nil, guisp = nil }
-  hi.Whitespace = { guifg = c.base17, guibg = nil, gui = nil, guisp = nil }
+  hi.Whitespace = { guifg = c.base11, guibg = nil, gui = nil, guisp = nil }
   hi.LineNr = { guifg = c.base03, guibg = nil, gui = nil, guisp = nil }
   hi.SignColumn = { guifg = c.base03, guibg = c.base00, gui = nil, guisp = nil }
   hi.StatusLine = { guifg = c.base04, guibg = c.base02, gui = "none", guisp = nil }
