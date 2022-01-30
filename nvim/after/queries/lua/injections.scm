20fe7a03dc9a4c3f732d080a293c9c8d056406be
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-01-30 11:24:23 -0600

    fix(nvim): disable vestigial custom lua injections

--- nvim/after/queries/lua/injections.scm
+++ nvim/after/queries/lua/injections.scm
@@ -1,23 +1,23 @@
-(
-	(function_call
-		(field_expression) @_vimcmd_identifier
-		(arguments
-			(string) @vim)
-  )
-
-	(#any-of? @_vimcmd_identifier "vim.cmd" "vim.api.nvim_command" "vim.api.nvim_exec")
-	(#match? @vim "^[\"']")
-	(#offset! @vim 0 1 0 -1)
-)
-
-(
-	(function_call
-		(field_expression) @_vimcmd_identifier
-		(arguments
-			(string) @vim)
-	)
-
-	(#any-of? @_vimcmd_identifier "vim.cmd" "vim.api.nvim_command" "vim.api.nvim_exec")
-	; (#match? @vim "^\\[\\[")
-	(#offset! @vim 0 2 0 -2)
-)
+; (
+; 	(function_call
+; 		(field_expression) @_vimcmd_identifier
+; 		(arguments
+; 			(string) @vim)
+;   )
+;
+; 	(#any-of? @_vimcmd_identifier "vim.cmd" "vim.api.nvim_command" "vim.api.nvim_exec")
+; 	(#match? @vim "^[\"']")
+; 	(#offset! @vim 0 1 0 -1)
+; )
+;
+; (
+; 	(function_call
+; 		(field_expression) @_vimcmd_identifier
+; 		(arguments
+; 			(string) @vim)
+; 	)
+;
+; 	(#any-of? @_vimcmd_identifier "vim.cmd" "vim.api.nvim_command" "vim.api.nvim_exec")
+; 	; (#match? @vim "^\\[\\[")
+; 	(#offset! @vim 0 2 0 -2)
+; )
