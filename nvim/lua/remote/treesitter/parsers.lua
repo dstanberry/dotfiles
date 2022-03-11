5dfe5e0d42e2987bea930fff5fc4564a585df2da
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-03-11 09:22:43 -0600

    fix(nvim): change treesitter highlight scheme for zsh

--- nvim/lua/remote/treesitter/parsers.lua
+++ nvim/lua/remote/treesitter/parsers.lua
@@ -1,13 +1,7 @@
 -- verify tree-sitter is available
-local ok, parsers = pcall(require, "nvim-treesitter.parsers")
+local ok, treesitter_parsers = pcall(require, "nvim-treesitter.parsers")
 if not ok then
   return
 end
 
-local ft_to_lang = parsers.ft_to_lang
-require("nvim-treesitter.parsers").ft_to_lang = function(ft)
-  if ft == "zsh" then
-    return "bash"
-  end
-  return ft_to_lang(ft)
-end
+treesitter_parsers.filetype_to_parsername.zsh = "bash"
