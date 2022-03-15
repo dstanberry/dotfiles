b0f70934c0055d7e69a0b79e6172bae3928ffe78
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-03-15 17:01:23 -0500

    fix(nvim): use `vim.opt_local` in ftplugin

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,15 +3,14 @@ local markdown = require "ft.markdown"
 local c = require("ui.theme").colors
 local groups = require "ui.theme.groups"
 
-vim.bo.formatlistpat = [=[^\s*\d\+\.\s\+\|^\s*[-*+>]\s\+\|^\[^\ze[^\]]\+\]:]=]
-vim.bo.iskeyword = vim.bo.iskeyword .. ",-"
-vim.bo.iskeyword = vim.bo.iskeyword .. ",@-@"
-vim.wo.breakindent = true
-vim.wo.breakindentopt = "min:5,list:-1"
-vim.wo.concealcursor = "c"
-vim.wo.conceallevel = 2
--- vim.wo.spell = true
-vim.wo.wrap = true
+vim.opt_local.formatlistpat = [=[^\s*\d\+\.\s\+\|^\s*[-*+>]\s\+\|^\[^\ze[^\]]\+\]:]=]
+vim.opt_local.iskeyword:append("-")
+vim.opt_local.breakindent = true
+vim.opt_local.breakindentopt = "min:5,list:-1"
+vim.opt_local.concealcursor = "c"
+vim.opt_local.conceallevel = 2
+-- vim.opt_local.spell = true
+vim.opt_local.wrap = true
 
 vim.keymap.set("i", "<c-w>c", markdown.insert_checkbox, { buffer = vim.api.nvim_get_current_buf() })
 vim.keymap.set("i", "<c-w>p", markdown.insert_link, { buffer = vim.api.nvim_get_current_buf() })
