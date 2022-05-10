a45f3114a79616e27007e30c4e3d0c6f09906d5c
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-05-10 16:06:30 -0500

    fix(nvim): only jump through nodes if inside snippet

--- nvim/lua/remote/luasnip/keymap.lua
+++ nvim/lua/remote/luasnip/keymap.lua
@@ -1,7 +1,7 @@
 local luasnip = require "remote.luasnip"
 
 vim.keymap.set({ "i", "s" }, "<tab>", function()
-  if luasnip.expand_or_jumpable() then
+  if luasnip.expand_or_locally_jumpable() then
     luasnip.expand_or_jump()
   else
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<tab>", true, true, true), "n", true)
@@ -9,7 +9,7 @@ vim.keymap.set({ "i", "s" }, "<tab>", function()
 end, { silent = true })
 
 vim.keymap.set({ "i", "s" }, "<s-tab>", function()
-  if luasnip.jumpable(-1) then
+  if luasnip.in_snippet() and luasnip.jumpable(-1) then
     luasnip.jump(-1)
   else
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<s-tab>", true, true, true), "n", true)
