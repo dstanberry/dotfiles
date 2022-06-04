6fdd4e8f6e8d6e8b47a9920a080ce1634338efc4
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-06-04 18:23:42 -0500

    fix(nvim): disable nvim-pasta plugin until upstream more mature

--- nvim/after/plugin/pasta.lua
+++ nvim/after/plugin/pasta.lua
@@ -6,11 +6,11 @@ end
 
 local mappings = require "pasta.mappings"
 
-vim.keymap.set("n", "p", mappings.p)
-vim.keymap.set("n", "P", mappings.P)
-
-pasta.setup {
-  converters = {},
-  next_key = vim.api.nvim_replace_termcodes("<c-n>", true, true, true),
-  prev_key = vim.api.nvim_replace_termcodes("<c-p>", true, true, true),
-}
+-- vim.keymap.set("n", "p", mappings.p)
+-- vim.keymap.set("n", "P", mappings.P)
+--
+-- pasta.setup {
+--   converters = {},
+--   next_key = vim.api.nvim_replace_termcodes("<c-n>", true, true, true),
+--   prev_key = vim.api.nvim_replace_termcodes("<c-p>", true, true, true),
+-- }
