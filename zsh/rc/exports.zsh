69c0360269edd7e44573939eae5a3787c5a102ff
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-07-31 17:20:44 -0500

    style(zsh): update fzf look and feel

--- zsh/rc/exports.zsh
+++ zsh/rc/exports.zsh
@@ -90,11 +90,16 @@ if hash fzf 2> /dev/null; then
     --scroll-off=3
     --bind=ctrl-d:preview-down
     --bind=ctrl-f:preview-up
+    --bind=tab:toggle-out
+    --bind=shift-tab:toggle-in
+    --prompt=" "
+    --pointer=""
+    --marker=""
     --color=dark
     --color=fg:#bebebe,bg:-1,hl:#93b379
     --color=fg+:#dfe3ec,bg+:-1,hl+:#93b379
-    --color=info:#5f5f5f,prompt:#6f8fb4,pointer:#b04b57
-    --color=marker:#e5c179,spinner:#4c566a,header:#4c566a'
+    --color=info:#5f5f5f,prompt:#93b379,pointer:#bebebe
+    --color=marker:#b04b57,spinner:#4c566a,header:#4c566a'
 fi
 
 # define mocOS specific options
