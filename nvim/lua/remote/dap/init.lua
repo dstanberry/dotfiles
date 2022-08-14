13c737a8e1721a252199bdc9196772a7f8c910d1
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-08-14 15:14:00 -0500

    chore(nvim): fix file formatting

--- nvim/lua/remote/dap/init.lua
+++ nvim/lua/remote/dap/init.lua
@@ -61,10 +61,10 @@ M.setup = function()
       layouts = {
         {
           elements = {
-          { id = "scopes", size = 0.25 },
-          { id = "breakpoints", size = 0.25 },
-          { id = "stacks", size = 0.25 },
-          { id = "watches", size = 0.25 },
+            { id = "scopes", size = 0.25 },
+            { id = "breakpoints", size = 0.25 },
+            { id = "stacks", size = 0.25 },
+            { id = "watches", size = 0.25 },
           },
           size = 50,
           position = "left",
