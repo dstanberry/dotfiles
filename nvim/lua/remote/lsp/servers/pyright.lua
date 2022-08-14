eb3e84aea05d5d7e55fc0893bbc07f6712c9c67c
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-08-13 20:50:42 -0500

    refactor(nvim): attempt to locate virtualenv during pyright config

--- nvim/lua/remote/lsp/servers/pyright.lua
+++ nvim/lua/remote/lsp/servers/pyright.lua
@@ -1,6 +1,17 @@
 local M = {}
 
 M.config = {
+  before_init = function(params, config)
+    local ok, Path = pcall(require, "plenary.path")
+    if ok then
+      local venv = Path:new((config.root_dir:gsub("/", Path.path.sep)), ".venv")
+      if venv:joinpath("bin"):is_dir() then
+        config.settings.python.pythonPath = tostring(venv:joinpath("bin", "python"))
+      elseif has "win32" and venv:joinpath("Scripts"):is_dir() then
+        config.settings.python.pythonPath = tostring(venv:joinpath("Scripts", "python.exe"))
+      end
+    end
+  end,
   settings = {
     python = {
       autoImportCompletions = true,
