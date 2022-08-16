db5bdc6a2359fa69bcf46103c3c298a28be5426f
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-08-16 11:54:50 -0500

    refactor(nvim): update dap configuration for go files

--- nvim/lua/remote/dap/debuggers/go.lua
+++ nvim/lua/remote/dap/debuggers/go.lua
@@ -3,61 +3,34 @@ local dap = require "remote.dap"
 local M = {}
 
 M.setup = function()
-  dap.adapters.go = function(callback, config)
-    local stdout = vim.loop.new_pipe(false)
-    local handle
-    local pid_or_err
-    local opts = {
-      stdio = { nil, stdout },
-      args = { "dap", "-l", string.format("%s:%s", config.host, config.port) },
-      detached = true,
-    }
-    handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
-      stdout:close()
-      handle:close()
-      if code ~= 0 then
-        print("dlv exited with code", code)
-      end
-    end)
-    assert(handle, "Error running dlv: " .. tostring(pid_or_err))
-    stdout:read_start(function(err, chunk)
-      assert(not err, err)
-      if chunk then
-        vim.schedule(function()
-          require("dap.repl").append(chunk)
-        end)
-      end
-    end)
-    vim.defer_fn(function()
-      callback { type = "server", host = config.host, port = config.port }
-    end, 100)
-  end
+  dap.adapters.delve = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      command = "dlv",
+      args = { "dap", "-l", "127.0.0.1:${port}" },
+    },
+  }
   dap.configurations.go = {
     {
-      type = "go",
+      type = "delve",
       name = "Debug",
       request = "launch",
       program = "${file}",
-      host = "127.0.0.1",
-      port = 38697,
     },
     {
-      type = "go",
+      type = "delve",
       name = "Debug test",
       request = "launch",
       mode = "test",
       program = "${file}",
-      host = "127.0.0.1",
-      port = 38697,
     },
     {
-      type = "go",
+      type = "delve",
       name = "Debug test (go.mod)",
       request = "launch",
       mode = "test",
       program = "./${relativeFileDirname}",
-      host = "127.0.0.1",
-      port = 38697,
     },
   }
 end
