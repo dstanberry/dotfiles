4fbc84b049acd5a50f71edd1706101dfe9815768
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-08-26 11:05:44 -0500

    fix(nvim): resolve issue with launching server on windows

--- nvim/lua/remote/lsp/servers/ls_emmet.lua
+++ nvim/lua/remote/lsp/servers/ls_emmet.lua
@@ -1,17 +1,26 @@
+local bin_name = 'ls_emmet'
+local cmd = { bin_name, '--stdio' }
+
+if vim.fn.has 'win32' == 1 then
+  cmd = { 'cmd.exe', '/C', bin_name, '--stdio' }
+end
+
 local M = {}
 
 M.config = {
-    cmd = { "ls_emmet", "--stdio" },
+    cmd = cmd,
     filetypes = {
       "css",
       "html",
       "javascript",
       "javascriptreact",
+      'javascript.jsx',
       "less",
       "sass",
       "scss",
       "typescript",
       "typescriptreact",
+      'typescript.tsx',
       "xml",
     },
     root_dir = function(_)
