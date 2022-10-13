dae43f6dc4d9528da48eca7e9e292d249b4c314c
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-10-12 22:57:29 -0500

    refactor(nvim): update notify configuration

--- nvim/after/plugin/notify.lua
+++ nvim/after/plugin/notify.lua
@@ -4,12 +4,17 @@ if not ok then
   return
 end
 
-local telescope = require("telescope")
+local telescope = require "telescope"
 
 notify.setup {
-  stages = "slide",
-  timeout = 5000,
+  stages = "fade_in_slide_out",
+  timeout = 3000,
   background_colour = "Normal",
+  render = function(...)
+    local n = select(2, ...)
+    local style = n.title[1] == "" and "minimal" or "default"
+    require("notify.render")[style](...)
+  end,
 }
 
 vim.notify = notify
