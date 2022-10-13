1aac46340cbd0bbcfec949d79f7400899909d43e
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-10-13 12:28:08 -0500

    chore(nvim): code cleanup

--- nvim/plugin/input.lua
+++ nvim/plugin/input.lua
@@ -4,19 +4,13 @@ if not vim.ui then
 end
 
 local util = require "util"
-
-local window = util.window
-
-local options = {
-  prefix = " ❯ ",
-}
+local icons = require "ui.icons"
 
 local confirm = function(items, on_choice, key)
   local choice = tonumber(vim.fn.expand "<cword>")
   if key then
     choice = key
   end
-
   vim.api.nvim_win_close(0, true)
   on_choice(items[choice], choice)
 end
@@ -26,11 +20,9 @@ end
 --     on_confirm = { on_confirm, "function", false },
 --   }
 --   opts = opts or {}
---
 --   local lines = {}
 --   local title = opts.prompt
 --   lines = { title, string.rep(window.separator, 30), unpack(lines) }
---
 --   local bufnr, _ = window.popup_window {
 --     width = 30,
 --     lines = lines,
@@ -39,7 +31,7 @@ end
 --     input = true,
 --     prompt = {
 --       enable = true,
---       prefix = options.prefix,
+--       prefix = icons.misc.Prompt,
 --       highlight = "Float",
 --     },
 --     on_confirm = function()
@@ -63,12 +55,10 @@ vim.ui.select = function(items, opts, on_choice)
   for i, item in pairs(items) do
     table.insert(choices, string.format("[%d] %s", i, format_item(item)))
   end
-
   local title = table.remove(choices, 1)
-  local width = window.calculate_width(choices)
-  choices = { title, string.rep(window.separator, width), unpack(choices) }
-
-  local bufnr, _ = window.popup_window {
+  local width = util.window.calculate_width(choices)
+  choices = { title, string.rep(util.window.separator, width), unpack(choices) }
+  local bufnr, _ = util.window.popup_window {
     lines = choices,
     enter = true,
     set_cursor = true,
@@ -78,7 +68,6 @@ vim.ui.select = function(items, opts, on_choice)
   }
   vim.api.nvim_buf_add_highlight(bufnr, -1, "Title", 0, 0, #title)
   vim.api.nvim_buf_add_highlight(bufnr, -1, "FloatBorder", 1, 0, -1)
-
   for k, _ in ipairs(choices) do
     if k > 2 then
       vim.keymap.set("n", string.format("%d", k - 2), function()
