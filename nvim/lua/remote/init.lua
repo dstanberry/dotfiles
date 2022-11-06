8f4c0db1d159dd4b4d11fbba3609ae0b5079d5e9
Demaro Stanberry <demaro.stanberry@gmail.com>
2022-11-06 13:24:16 -0600

    style(nvim): remove border for packer floating windows

--- nvim/lua/remote/init.lua
+++ nvim/lua/remote/init.lua
@@ -8,7 +8,7 @@ local config = {
   },
   display = {
     open_fn = function()
-      return require("packer.util").float { border = "single" }
+      return require("packer.util").float { border = "none" }
     end,
   },
 }
