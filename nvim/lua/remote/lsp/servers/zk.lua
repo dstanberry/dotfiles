4e83018d64b5db95dee25174c222d70c78187488
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-02-24 14:48:24 -0600

    fix(nvim): sanitize `ZK_NOTEBOOK_DIR` environment variable

--- nvim/lua/remote/lsp/servers/zk.lua
+++ nvim/lua/remote/lsp/servers/zk.lua
@@ -3,22 +3,16 @@ if not ok then
   return
 end
 
-local M = {}
-
-vim.g.zk_notebook = vim.env.hash_notes and ("%s/zettelkasten/vault"):format(vim.env.hash_notes)
-local win_documents_path = ("%s/Documents"):format(vim.env.HOME)
+vim.env.ZK_NOTEBOOK_DIR = vim.env.hash_notes and ("%s/zettelkasten/vault"):format(vim.env.hash_notes)
+local win_documents_path = ("%s\\Documents"):format(vim.env.HOME)
 if has "win32" and vim.fn.empty(vim.fn.glob(win_documents_path)) > 0 then
   if vim.fn.empty(vim.fn.glob "D:\\Documents") == 0 then
     win_documents_path = "D:\\Documents"
   end
-  vim.g.zk_notebook = ("%s/_notes/zettelkasten/vault"):format(win_documents_path)
+  vim.env.ZK_NOTEBOOK_DIR = vim.fn.expand(("%s/_notes/zettelkasten/vault"):format(win_documents_path))
 end
 
-M.config = {
-  -- cmd = { "zk", "lsp", "--log", "/tmp/zk-lsp.log" },
-  cmd = { "zk", "lsp" },
-  name = "zk",
-}
+local M = {}
 
 M.setup = function(cfg)
   zk.setup {
@@ -33,5 +27,11 @@ M.setup = function(cfg)
   require "ft.markdown.commands"
   require "ft.markdown.keymaps"
 end
+
+M.config = {
+  -- cmd = { "zk", "lsp", "--log", "/tmp/zk-lsp.log" },
+  cmd = { "cmd.exe", "/C", "zk", "lsp" },
+  name = "zk",
+}
 
 return M
