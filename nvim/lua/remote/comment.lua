9fad86b1ab347687ee9b55b0fd5684ddb77533cd
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-02-25 11:59:16 -0600

    feat(nvim): enable `nvim-ts-context-commentstring` integration for `Comment.nvim`

--- nvim/lua/remote/comment.lua
+++ nvim/lua/remote/comment.lua
@@ -1,19 +1,24 @@
 return {
   "numToStr/Comment.nvim",
-  opts = {
-    padding = true,
-    sticky = true,
-    mappings = {
+  opts = function(_, opts)
+    local ok, ts_context_commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+    if ok then
+      opts.pre_hook = ts_context_commentstring.create_pre_hook()
+    end
+    opts.padding = true
+    opts.padding = true
+    opts.sticky = true
+    opts.mappings = {
       basic = true,
       extra = true,
-    },
-    opleader = {
+    }
+    opts.opleader = {
       line = "gc",
       block = "gb",
-    },
-    toggler = {
+    }
+    opts.toggler = {
       line = "gcc",
       block = "gbc",
-    },
-  },
+    }
+  end,
 }
