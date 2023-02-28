7c42f61316a8c3a142fc77a3904fdeade829a95f
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-02-27 23:46:23 -0600

    fix(nvim): update deprecated configuration

--- nvim/lua/remote/persisted.lua
+++ nvim/lua/remote/persisted.lua
@@ -1,7 +1,7 @@
 return {
   "olimorris/persisted.nvim",
-  lazy = false,
   dependencies = { "nvim-telescope/telescope.nvim" },
+  lazy = false,
   init = function()
     vim.api.nvim_create_augroup("sessionmgr", { clear = true })
     vim.api.nvim_create_autocmd("User", {
@@ -10,6 +10,23 @@ return {
       callback = function()
         -- stylua: ignore
         vim.schedule(function() vim.cmd "%bd" end)
+      end,
+    })
+    vim.api.nvim_create_autocmd("User", {
+      group = "sessionmgr",
+      pattern = "PersistedTelescopeLoadPost",
+      callback = function(session)
+        vim.notify("Loaded session " .. session.data.name, vim.log.levels.INFO, { title = title })
+        local path = session.data.dir_path
+        if string.find(path, "/") ~= 1 then
+          vim.cmd.cd(vim.fn.expand "~" .. "/" .. path)
+          vim.cmd.tcd(vim.fn.expand "~" .. "/" .. path)
+        else
+          vim.cmd.cd(path)
+          vim.cmd.tcd(path)
+        end
+        -- stylua: ignore
+        vim.schedule(function() vim.cmd.edit "%" end)
       end,
     })
     require("telescope").load_extension "persisted"
@@ -26,17 +43,5 @@ return {
     use_git_branch = true,
     allowed_dirs = { vim.g.dotfiles, vim.g.work_dir },
     ignored_dirs = { vim.fn.stdpath "data" },
-    telescope = {
-      after_source = function(param)
-        local path = param.dir_path
-        if string.find(path, "/") ~= 1 then
-          vim.api.nvim_command("cd " .. vim.fn.expand "~" .. "/" .. path)
-          vim.api.nvim_command("tcd " .. vim.fn.expand "~" .. "/" .. path)
-        else
-          vim.api.nvim_command("cd " .. path)
-          vim.api.nvim_command("tcd " .. path)
-        end
-      end,
-    },
   },
 }
