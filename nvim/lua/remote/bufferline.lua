371dfe55f53e785555d632e0854f7738926c4987
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-03-22 11:24:36 -0500

    chore(nvim): clean up `bufferline` configuration

--- nvim/lua/remote/bufferline.lua
+++ nvim/lua/remote/bufferline.lua
@@ -7,15 +7,13 @@ groups.new("PanelHeading", { link = "Title" })
 
 return {
   "akinsho/bufferline.nvim",
-  dependencies = {
-    "tiagovla/scope.nvim",
-    config = function() require("scope").setup() end,
-  },
+  dependencies = { "tiagovla/scope.nvim", config = true },
+  event = "VimEnter",
   config = function()
-    local bgroups = require "bufferline.groups"
+    local bufferline_groups = require "bufferline.groups"
     require("bufferline").setup {
+      ---@diagnostic disable-next-line: assign-type-mismatch
       highlights = function(defaults)
-        local diagnostic = vim.regex [[\(error_selected\|warning_selected\|info_selected\|hint_selected\)]]
         local hl = util.map(function(agg, attrs, name)
           local formatted = name:lower()
           local is_group = formatted:match "group"
@@ -23,10 +21,8 @@ return {
           local is_separator = formatted:match "separator"
           local is_selected = formatted:match "selected"
           local is_visible = formatted:match "visible"
-          if diagnostic and diagnostic:match_str(formatted) then attrs.fg = c.fg1 end
           if not is_group or (is_group and is_separator) then attrs.bg = c.bg2 end
-          if not is_group and not is_offset and is_separator then attrs.fg = c.bgX end
-          if is_group and not is_offset then attrs.bg = c.red1 end
+          if is_separator and not (is_group or is_offset) then attrs.fg = c.bgX end
           if is_selected or is_visible then attrs.bg = c.bgX end
           agg[name] = attrs
           return agg
@@ -44,6 +40,7 @@ return {
         mode = "buffers",
         numbers = "none",
         left_mouse_command = "buffer %d",
+        ---@diagnostic disable-next-line: assign-type-mismatch
         right_mouse_command = nil,
         middle_mouse_command = "bdelete! %d",
         close_command = "bdelete! %d",
@@ -53,6 +50,14 @@ return {
         left_trunc_marker = icons.misc.LeftArrowCircled,
         right_trunc_marker = icons.misc.RightArrowCircled,
         max_name_length = 20,
+        color_icons = true,
+        show_buffer_close_icons = true,
+        show_close_icon = false,
+        show_buffer_default_icon = true,
+        show_tab_indicators = true,
+        separator_style = "thin",
+        always_show_bufferline = true,
+        sort_by = "insert_after_current",
         diagnostics = "nvim_lsp",
         diagnostics_update_in_insert = false,
         diagnostics_indicator = function(_, _, _, ctx)
@@ -86,14 +91,6 @@ return {
             text_align = "center",
           },
         },
-        color_icons = true,
-        show_buffer_close_icons = true,
-        show_close_icon = false,
-        show_buffer_default_icon = true,
-        show_tab_indicators = true,
-        separator_style = "thin",
-        always_show_bufferline = true,
-        sort_by = "insert_after_current",
         groups = {
           items = {
             {
@@ -101,9 +98,10 @@ return {
               icon = icons.groups.Sql,
               auto_close = true,
               highlight = { fg = c.orange0 },
+              ---@diagnostic disable-next-line: undefined-field
               matcher = function(buf) return buf.filename:match "%.sql$" end,
               separator = {
-                style = bgroups.separator.pill,
+                style = bufferline_groups.separator.pill,
               },
             },
             {
@@ -112,11 +110,12 @@ return {
               highlight = { fg = c.yellow0 },
               auto_close = true,
               matcher = function(buf)
+                ---@diagnostic disable-next-line: undefined-field
                 local name = buf.filename
                 return name:match "_spec" or name:match ".spec" or name:match "_test" or name:match ".test"
               end,
               separator = {
-                style = bgroups.separator.pill,
+                style = bufferline_groups.separator.pill,
               },
             },
             {
@@ -126,7 +125,7 @@ return {
               auto_close = true,
               matcher = function(buf) return vim.startswith(buf.path, vim.env.ZK_NOTEBOOK_DIR) or buf.path:match "zettelkasten" end,
               separator = {
-                style = bgroups.separator.pill,
+                style = bufferline_groups.separator.pill,
               },
             },
           },
