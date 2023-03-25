564cd561b816df57429d3d04ae7c1ae2f1859ce4
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-03-24 21:55:54 -0500

    style(nvim): dim `todo-comments`

--- nvim/lua/remote/todo-comments.lua
+++ nvim/lua/remote/todo-comments.lua
@@ -1,3 +1,6 @@
+local c = require("ui.theme").colors
+local color = require "util.color"
+
 return {
   "folke/todo-comments.nvim",
   cmd = { "TodoQuickFix", "TodoTelescope" },
@@ -6,5 +9,13 @@ return {
   opts = {
     signs = false,
     sign_priority = 0,
+    colors = {
+      error = { color.blend(c.red1, c.gray1, "50") },
+      warning = { color.blend(c.yellow2, c.gray1, "50") },
+      info = { color.blend(c.aqua1, c.gray1, "50") },
+      hint = { color.blend(c.magenta1, c.gray1, "50") },
+      default = { color.blend(c.blue0, c.gray1, "50") },
+      test = { color.blend(c.green0, c.gray1, "50") },
+    },
   },
 }
