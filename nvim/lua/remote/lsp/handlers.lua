67d3964f34836c5c6626435c278af8595631e04f
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-04-07 18:59:26 -0500

    chore(nvim): enable `completionitem.*` lsp options

--- nvim/lua/remote/lsp/handlers.lua
+++ nvim/lua/remote/lsp/handlers.lua
@@ -151,6 +151,11 @@ end
 M.get_client_capabilities = function()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
+  capabilities.textDocument.completion.completionItem.preselectSupport = true
+  capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+  capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+  capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+  capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
   capabilities.textDocument.completion.completionItem.resolveSupport = {
     properties = { "documentation", "detail", "additionalTextEdits" },
   }
