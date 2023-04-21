774ce190434464f6ab6b08499e654d8cc24e85db
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-04-21 10:29:35 -0500

    chore(nvim): change `keywordprg` for lua files

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,2 +1,3 @@
+vim.bo.keywordprg = ":help"
 vim.opt_local.include = [=[\v<((do|load)file|require)\s*\(?['"]\zs[^'"]+\ze['"]]=]
 vim.opt_local.includeexpr = [[v:lua.require("ft.lua").include_expr(v:fname)]]
