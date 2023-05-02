9e3fab67593b4082dcf188b5380c02e50de18886
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-05-02 10:26:10 -0500

    chore(nvim): encapsulate python path

--- nvim/lua/remote/dap/debuggers/python.lua
+++ nvim/lua/remote/dap/debuggers/python.lua
@@ -1,13 +1,13 @@
 local M = {}
 
-M.setup = function(cmd)
+M.setup = function()
   -- verify dap is available
   local ok, dap_python = pcall(require, "dap-python")
   if not ok then return end
 
   local dap = require "dap"
 
-  cmd = has "win32" and vim.fn.exepath "python" or "python"
+  local cmd = has "win32" and vim.fn.exepath "python" or "python"
   dap.configurations.python = {}
   dap_python.setup(cmd, {
     include_configs = true,
