ab7c901f6a4609850bfcd8ca41ed79a4280c8374
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-09-15 11:38:08 -0500

    chore(nvim): switch `treesitter-rainbow` to maintained version

--- nvim/lua/remote/treesitter/init.lua
+++ nvim/lua/remote/treesitter/init.lua
@@ -1,21 +1,3 @@
-local groups = require "ui.theme.groups"
-
-local CYAN = "#73c1b9"
-local CYAN_LIGHT = "#80d3dd"
-local PINK = "#dec7d0"
-local ORANGE = "#e09696"
-local YELLOW = "#ffdca8"
-local MAGENTA = "#9086a4"
-local MAGENTA_LIGHT = "#bfafc4"
-
-groups.new("TSRainbow1", { fg = CYAN })
-groups.new("TSRainbow2", { fg = ORANGE })
-groups.new("TSRainbow3", { fg = MAGENTA })
-groups.new("TSRainbow4", { fg = CYAN_LIGHT })
-groups.new("TSRainbow5", { fg = PINK })
-groups.new("TSRainbow6", { fg = MAGENTA_LIGHT })
-groups.new("TSRainbow7", { fg = YELLOW })
-
 vim.treesitter.language.register("bash", "zsh")
 vim.treesitter.language.register("typescript", "typescriptreact")
 
@@ -39,7 +21,6 @@ return {
           load_textobjects = true
         end,
       },
-      "HiPhish/nvim-ts-rainbow2",
       "JoosepAlviste/nvim-ts-context-commentstring",
       "nvim-treesitter/playground",
       "theHamsta/nvim-treesitter-pairs",
@@ -98,20 +79,6 @@ return {
         enable = true,
         use_virtual_text = true,
         lint_events = { "BufWrite", "CursorHold" },
-      },
-      rainbow = {
-        enable = true,
-        query = "rainbow-parens",
-        disable = { "typescriptreact" },
-        hlgroups = {
-          "TSRainbow1",
-          "TSRainbow2",
-          "TSRainbow3",
-          "TSRainbow4",
-          "TSRainbow5",
-          "TSRainbow6",
-          "TSRainbow7",
-        },
       },
     },
     config = function(_, opts)
