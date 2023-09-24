343c39b30aa319644dede4a04214ff06ceabf876
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-09-23 21:08:08 -0500

    chore(nvim): clean up stale/deprecated references

--- nvim/lua/remote/lualine/components/languageservers.lua
+++ nvim/lua/remote/lualine/components/languageservers.lua
@@ -6,9 +6,9 @@ return function()
   local buf = vim.api.nvim_win_get_buf(winid)
   local limit = math.floor(0.15 * vim.fn.winwidth(winid))
   local clients = util.map(function(names, client, _)
-    if client.name and client.name ~= "null-ls" then table.insert(names, client.name) end
+    if client.name then table.insert(names, client.name) end
     return names
-  end, vim.lsp.get_active_clients { bufnr = buf })
+  end, vim.lsp.get_clients { bufnr = buf })
 
   local names = clients and table.concat(clients, pad(icons.misc.CircleDot, "both")) or ""
   return #names == 0 and ""
