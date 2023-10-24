272ab25baa3afeb62bc5fcb6e4ed698f6c59fba3
Demaro Stanberry <demaro.stanberry@gmail.com>
2023-10-24 10:35:19 -0500

    feat(nvim): add helper to display scollback buffer

--- nvim/lua/util/window.lua
+++ nvim/lua/util/window.lua
@@ -65,4 +65,33 @@ function M.popup_window(opts)
   return bufnr, winnr
 end
 
+function M.term_scrollback()
+  vim.wo.number = false
+  vim.wo.relativenumber = false
+  vim.wo.statuscolumn = ""
+  vim.wo.signcolumn = "no"
+  vim.opt.listchars = { space = " " }
+
+  local buf = vim.api.nvim_get_current_buf()
+
+  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+  while #lines > 0 and vim.trim(lines[#lines]) == "" do
+    lines[#lines] = nil
+  end
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, {})
+
+  vim.api.nvim_chan_send(vim.api.nvim_open_term(buf, {}), table.concat(lines, "\r\n"))
+
+  vim.keymap.set("n", "q", function() vim.cmd.quitall { bang = true } end, { silent = true, buffer = buf })
+
+  vim.api.nvim_create_autocmd("TextChanged", {
+    buffer = buf,
+    callback = function() vim.cmd.normal { args = { "G$" }, bang = true } end,
+  })
+  vim.api.nvim_create_autocmd("TermEnter", {
+    buffer = buf,
+    callback = function() vim.cmd.stopinsert() end,
+  })
+end
+
 return M
