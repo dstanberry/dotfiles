d6f28194dfd51ead25ee474cbf270f89aa70617f
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-01-19 14:32:00 -0600

    chore(nvim): change `git-worktree.nvim` to new maintainer

--- nvim/lua/remote/git-worktree.lua
+++ nvim/lua/remote/git-worktree.lua
@@ -1,24 +1,37 @@
 return {
-  "ThePrimeagen/git-worktree.nvim",
+  "polarmutex/git-worktree.nvim",
   event = "VeryLazy",
-  keys = {
-    {
-      "<leader>gl",
-      function() require("telescope").extensions.git_worktree.git_worktrees() end,
-      desc = "worktree: list linked tree(s)",
-    },
-    -- NOTE: creating new worktrees via cli is more robust
-    -- {
-    --   "<leader>ga",
-    --   function() require("telescope").extensions.git_worktree.create_git_worktree() end,
-    --   desc = "worktree: create new tree",
-    -- },
-  },
   config = function()
     local has_telescope, telescope = pcall(require, "telescope")
     if not has_telescope then return end
 
+    local themes = require "telescope.themes"
+
     require("git-worktree").setup {}
     telescope.load_extension "git_worktree"
+
+    vim.keymap.set(
+      "n",
+      "<leader>gl",
+      function()
+        telescope.extensions.git_worktree.git_worktree(themes.get_dropdown {
+          previewer = false,
+          prompt_title = "Switch to a Git Working Tree",
+        })
+      end,
+      { desc = "git-worktree: switch to a worktree" }
+    )
+
+    vim.keymap.set(
+      "n",
+      "<leader>ga",
+      function()
+        telescope.extensions.git_worktree.create_git_worktree(themes.get_dropdown {
+          previewer = false,
+          prompt_title = "Create a Git Working Tree",
+        })
+      end,
+      { desc = "git-worktree: create a worktree" }
+    )
   end,
 }
