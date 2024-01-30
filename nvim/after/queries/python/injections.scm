2d2554c0f8d70241d76f8c929f3eb81e8e6f8f8b
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-01-30 11:58:58 -0600

    feat(nvim): add sql highlighting for python

---
+++ nvim/after/queries/python/injections.scm
@@ -0,0 +1,11 @@
+; extends
+
+(assignment
+  left: (identifier) @_id (#contains? @_id "query")
+  right: (string (string_content) @injection.content
+     (#set! injection.language "sql")))
+
+(assignment
+  left: (identifier) @_id (#contains? @_id "sql")
+  right: (string (string_content) @injection.content
+     (#set! injection.language "sql")))
