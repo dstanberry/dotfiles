8205bcc7d566c0718d830bedcf16588ab802bb67
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-02-28 08:36:26 -0600

    chore(nvim): update pyright configuration

--- nvim/lua/remote/lsp/servers/pyright.lua
+++ nvim/lua/remote/lsp/servers/pyright.lua
@@ -1,18 +1,6 @@
 local M = {}
 
 M.config = {
-  before_init = function(_, config)
-    local ok, Path = pcall(require, "plenary.path")
-    if ok then
-      local venv = Path:new((config.root_dir:gsub("/", Path.path.sep)), ".venv")
-      if venv:joinpath("bin"):is_dir() then
-        config.settings.python.pythonPath = tostring(venv:joinpath("bin", "python"))
-      elseif has "win32" and venv:joinpath("Scripts"):is_dir() then
-        config.settings.python.pythonPath = tostring(venv:joinpath("Scripts", "python.exe"))
-      end
-      require("remote.dap.debuggers.python").setup(config.settings.python.pythonPath)
-    end
-  end,
   settings = {
     python = {
       autoImportCompletions = true,
@@ -28,7 +16,7 @@ M.config = {
         local items = result.items or result
         items = require("util").map(function(acc, item)
           if
-            not (item.data and item.data.funcParensDisabled)
+            (item.data and item.data.funcParensDisabled)
             and (
               item.kind == vim.lsp.protocol.CompletionItemKind.Constructor
               or item.kind == vim.lsp.protocol.CompletionItemKind.Function
