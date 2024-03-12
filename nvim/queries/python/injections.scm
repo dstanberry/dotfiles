79f10c469ae9d398003b82d8653296b4f5b65968
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-03-12 17:33:17 -0500

    fix(nvim): improve sql injection in python files

--- nvim/queries/python/injections.scm
+++ nvim/queries/python/injections.scm
@@ -8,4 +8,7 @@
 (assignment
   left: (identifier) @_id (#contains? @_id "sql")
   right: (string (string_content) @injection.content
-     (#set! injection.language "sql")))
+	(#set! injection.language "sql")
+	(#set! injection.include-children)
+	(#set! injection.combined)
+))
