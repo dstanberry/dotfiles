65c4ab412a43bcaf1a99a53e0def15c75b666595
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-03-18 17:43:49 -0500

    chore(nvim): re-enable markdown formatters

--- nvim/lua/remote/conform.lua
+++ nvim/lua/remote/conform.lua
@@ -22,7 +22,7 @@ return {
       json = { { "prettierd", "prettier" } },
       jsonc = { { "prettierd", "prettier" } },
       lua = { "stylua" },
-      -- markdown = { "markdownlint", "cbfmt" },
+      markdown = { "markdownlint", "cbfmt" },
       pgsql = { "sql_formatter" },
       -- python = { "isort", "black", "injected" },
       python = { "isort", "black" },
