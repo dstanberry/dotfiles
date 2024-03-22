0edfd26bc9abfd0550aa77087c2d7c1aa3c3f93e
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-03-22 11:22:06 -0500

    feat(nvim): add mechanism to open angular components/templates

--- nvim/lua/remote/lsp/servers/angularls.lua
+++ nvim/lua/remote/lsp/servers/angularls.lua
@@ -1,23 +1,26 @@
 local util = require "util"
 
-local path = vim.fs.normalize(string.format("%s/mason/packages", vim.fn.stdpath "data"))
-local install_dir =
-  vim.fs.normalize(string.format("%s/angular-language-server/node_modules/@angular/language-server", path))
+local angularls_path = vim.fs.normalize(
+  string.format(
+    "%s/angular-language-server/node_modules/@angular/language-server",
+    vim.fs.normalize(string.format("%s/mason/packages", vim.fn.stdpath "data"))
+  )
+)
 
 local node_modules = function(dirs)
   return util.map(dirs, function(dir) return table.concat({ dir, "node_modules" }, "/") end)
 end
 
-local function get_cmd(workspace_dir)
+local get_cmd = function(workspace_dir)
   local cmd = {
     "ngserver",
     "--stdio",
     "--tsProbeLocations",
-    table.concat(node_modules { install_dir, workspace_dir }, ","),
+    table.concat(node_modules { angularls_path, workspace_dir }, ","),
     "--ngProbeLocations",
     table.concat(
       node_modules {
-        table.concat({ install_dir, "node_modules", "@angular", "language-service" }, "/"),
+        table.concat({ angularls_path, "node_modules", "@angular", "language-service" }, "/"),
         workspace_dir,
       },
       ","
@@ -30,12 +33,41 @@ local function get_cmd(workspace_dir)
   return cmd
 end
 
+local goto_template = function()
+  local params = vim.lsp.util.make_position_params(0)
+  vim.lsp.buf_request(0, "angular/getTemplateLocationForComponent", params, function(_, result)
+    if result then vim.lsp.util.jump_to_location(result, "utf-8") end
+  end)
+end
+
+local goto_component = function()
+  local params = vim.lsp.util.make_position_params(0)
+  vim.lsp.buf_request(0, "angular/getComponentsWithTemplateFile", params, function(_, result)
+    if result then
+      if #result == 1 then
+        vim.lsp.util.jump_to_location(result[1], "utf-8")
+      else
+        vim.fn.setqflist({}, " ", {
+          title = "Angular Component Files",
+          items = vim.lsp.util.locations_to_items(result, "utf-8"),
+        })
+        vim.cmd.copen()
+      end
+    end
+  end)
+end
+
 local M = {}
 
 M.config = {
   cmd = get_cmd(vim.uv.cwd()),
   on_new_config = function(new_config, root_dir) new_config.cmd = get_cmd(root_dir) end,
-  on_attach = function() end,
+  on_attach = function(client)
+    client.server_capabilities.renameProvider = false
+
+    vim.api.nvim_create_user_command("NgTemplate", goto_template, {})
+    vim.api.nvim_create_user_command("NgComponent", goto_component, {})
+  end,
 }
 
 return M
