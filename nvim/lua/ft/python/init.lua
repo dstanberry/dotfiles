d9eedc5f3d3108b46f8f121148ad60de62cb621b
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-03-25 11:41:18 -0500

    fix(nvim): resolve import loop

--- nvim/lua/ft/python/init.lua
+++ nvim/lua/ft/python/init.lua
@@ -1,5 +1,3 @@
-local python = require "ft.python"
-
 local M = {}
 
 local NAMESPACE_ID = vim.api.nvim_create_namespace "python_ns_extmarks"
@@ -9,9 +7,22 @@ M.disable_extmarks = function()
   pcall(vim.api.nvim_buf_clear_namespace, 0, NAMESPACE_ID, line - 1, line + 1)
 end
 
+M.parse_document = function()
+  local language_tree = vim.treesitter.get_parser(0, "python")
+  local syntax_tree = language_tree:parse()
+  local root = syntax_tree[1]:root()
+  local parsed_query = vim.treesitter.query.parse(
+    "python",
+    [[
+      (string_start) @string_init
+    ]]
+  )
+  return root, parsed_query
+end
+
 M.set_extmarks = function()
   vim.api.nvim_buf_clear_namespace(0, NAMESPACE_ID, 0, -1)
-  local root, parsed_query = python.parse_document()
+  local root, parsed_query = M.parse_document()
   for _, captures, metadata in parsed_query:iter_matches(root, 0) do
     for id, node in pairs(captures) do
       local capture = parsed_query.captures[id]
