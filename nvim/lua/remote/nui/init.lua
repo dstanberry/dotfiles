afcbecd9e84c204e72ee9a7699a97efc673ea1c1
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-04-01 09:14:18 -0500

    feat(nvim): test-drive `nui-components.nvim`

---
+++ nvim/lua/remote/nui/init.lua
@@ -0,0 +1,31 @@
+local c = require("ui.theme").colors
+local color = require "util.color"
+local groups = require "ui.theme.groups"
+
+groups.new("NuiCursorLine", {})
+groups.new("NuiLabel", { fg = c.white })
+groups.new("NuiNormalFloat", { fg = c.white })
+groups.new("NuiGapFloat", {})
+groups.new("NuiResultsFloat", {})
+groups.new("NuiFloatBorder", { fg = color.blend(c.gray1, c.blue0, 0.44) })
+groups.new("NuiComponentsButton", { link = "NormalSB" })
+groups.new("NuiComponentsButtonActive", { link = "PMenuSel" })
+groups.new("NuiComponentsCheckboxIcon", { fg = c.fg0 })
+groups.new("NuiComponentsCheckboxIconChecked", { fg = c.magenta2, bold = true })
+groups.new("NuiComponentsCheckboxLabel", { link = "NuiComponentsCheckboxIcon" })
+groups.new("NuiComponentsCheckboxLabelChecked", { link = "NuiComponentsCheckboxIconChecked" })
+
+return {
+  "grapp-dev/nui-components.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  event = { "LazyFile", "VeryLazy" },
+  keys = {
+    {
+      "<localleader>sr",
+      function() require("remote.nui.pickers.spectre").toggle() end,
+      desc = "spectre: replace in files",
+    },
+  },
+}
