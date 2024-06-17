848a9d139cd2476873dd5969b9528a4843f47d7b
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-06-17 09:00:13 -0500

    feat(nvim): add keymap to move between fn parameters

--- nvim/lua/remote/treesitter/init.lua
+++ nvim/lua/remote/treesitter/init.lua
@@ -105,10 +105,10 @@ return {
       textobjects = {
         move = {
           enable = true,
-          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+          goto_next_start = { ["]a"] = "@parameter.inner", ["]c"] = "@class.outer", ["]f"] = "@function.outer" },
+          goto_next_end = { ["]A"] = "@parameter.inner", ["]C"] = "@class.outer", ["]F"] = "@function.outer" },
+          goto_previous_start = { ["[a"] = "@parameter.inner", ["[c"] = "@class.outer", ["[f"] = "@function.outer" },
+          goto_previous_end = { ["[A"] = "@parameter.inner", ["[C"] = "@class.outer", ["[F"] = "@function.outer" },
         },
       },
       query_linter = {
