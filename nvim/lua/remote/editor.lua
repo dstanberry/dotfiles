cc8ef81e5e5e2f9090e68d357040d321296212a3
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-06-17 10:12:38 -0500

    feat(nvim): add `mini.pairs` plugin

--- nvim/lua/remote/editor.lua
+++ nvim/lua/remote/editor.lua
@@ -369,6 +369,64 @@ return {
     end,
   },
   {
+    "echasnovski/mini.pairs",
+    event = "LazyFile",
+    opts = {
+      markdown = true,
+      modes = { insert = true, command = true, terminal = false },
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      skip_ts = { "string" },
+      skip_unbalanced = true,
+    },
+    config = function(_, opts)
+      local pairs = require "mini.pairs"
+      pairs.setup(opts)
+      local open = pairs.open
+      pairs.open = function(pair, neigh_pattern)
+        if vim.fn.getcmdline() ~= "" then return open(pair, neigh_pattern) end
+        local _o, _c = pair:sub(1, 1), pair:sub(2, 2)
+        local line = vim.api.nvim_get_current_line()
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        local next = line:sub(cursor[2] + 1, cursor[2] + 1)
+        local before = line:sub(1, cursor[2])
+        if opts.markdown and _o == "`" and vim.bo.filetype == "markdown" and before:match "^%s*``" then
+          return "`\n```" .. vim.api.nvim_replace_termcodes("<up>", true, true, true)
+        end
+        if opts.skip_next and next ~= "" and next:match(opts.skip_next) then return _o end
+        if opts.skip_ts and #opts.skip_ts > 0 then
+          local ok, captures = pcall(vim.treesitter.get_captures_at_pos, 0, cursor[1] - 1, math.max(cursor[2] - 1, 0))
+          for _, capture in ipairs(ok and captures or {}) do
+            if vim.tbl_contains(opts.skip_ts, capture.capture) then return _o end
+          end
+        end
+        if opts.skip_unbalanced and next == _c and _c ~= _o then
+          local _, count_open = line:gsub(vim.pesc(pair:sub(1, 1)), "")
+          local _, count_close = line:gsub(vim.pesc(pair:sub(2, 2)), "")
+          if count_close > count_open then return _o end
+        end
+        return open(pair, neigh_pattern)
+      end
+      vim.api.nvim_create_user_command("PairsDisable", function(args)
+        if args.bang then
+          vim.b.minipairs_disable = true
+        else
+          vim.g.minipairs_disable = true
+        end
+        print "Disabled auto pairs"
+      end, {
+        desc = "mini.pairs: disable auto pairs",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("PairsEnable", function()
+        vim.b.minipairs_disable = false
+        vim.g.minipairs_disable = false
+        print "Enabled auto pairs"
+      end, {
+        desc = "mini.pairs: enable auto pairs",
+      })
+    end,
+  },
+  {
     "echasnovski/mini.surround",
     keys = function(_, keys)
       local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
