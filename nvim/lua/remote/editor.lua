8391c147f2c820503ebc6102bcfd605e7e2ac34f
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-06-18 09:40:12 -0500

    fix(nvim): conflate <c-@> and <c-space>

--- nvim/lua/remote/editor.lua
+++ nvim/lua/remote/editor.lua
@@ -547,6 +547,7 @@ return {
           -- ["<c-d>"] = cmp.mapping.scroll_docs(4),
           -- ["<c-f>"] = cmp.mapping.scroll_docs(-4),
           ["<c-space>"] = cmp.mapping.complete(),
+          ["<c-@>"] = cmp.mapping.complete(),
           ["<c-c>"] = cmp.mapping.close(),
           ["<cr>"] = cmp.mapping.confirm { select = true },
         },
