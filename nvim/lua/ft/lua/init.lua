244781fed6f838e319724069fe4eb03b73dd890d
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-06-22 10:45:22 -0500

    chore(nvim): simplify `includeexpr`

--- nvim/lua/ft/lua/init.lua
+++ nvim/lua/ft/lua/init.lua
@@ -1,12 +1,4 @@
-local dir_separator = function()
-  if package.config then
-    return string.match(package.config, "^[^\n]")
-  elseif ds.has "win32" then
-    return "\\"
-  else
-    return "/"
-  end
-end
+local sep = string.match(package.config, "^[^\n]")
 
 local include_paths = function(fname, ext)
   ext = ext or "lua"
@@ -19,7 +11,6 @@ end
 
 local include_rtpaths = function(fname, ext)
   ext = ext or "lua"
-  local sep = dir_separator()
   local rtpaths = vim.api.nvim_list_runtime_paths()
   local modfile, initfile = string.format("%s.%s", fname, ext), string.format("init.%s", ext)
   for _, path in ipairs(rtpaths) do
@@ -33,8 +24,7 @@ end
 local M = {}
 
 M.include_expr = function(module)
-  local sep = string.match(package.config, "^[^\n]")
-  local fname = vim.fn.substitute(module, "\\.", sep, "g")
+  local fname = module:gsub("%.", sep)
   local f
   f = include_paths(fname, "lua")
   if f then return f end
