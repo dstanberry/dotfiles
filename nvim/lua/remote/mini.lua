2546e07253227b0f96031e52c71f0b3398a08c69
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-07-02 12:26:37 -0500

    feat(nvim): add `mini.hipatterns` and `mini.align`

---
+++ nvim/lua/remote/mini.lua
@@ -0,0 +1,389 @@
+return {
+  {
+    "echasnovski/mini.ai",
+    event = "LazyFile",
+    opts = function()
+      local ai = require "mini.ai"
+
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }, {}),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+          d = { "%f[%d]%d+" }, -- digits
+          e = { -- word with case
+            {
+              "%u[%l%d]+%f[^%l%d]",
+              "%f[%S][%l%d]+%f[^%l%d]",
+              "%f[%P][%l%d]+%f[^%l%d]",
+              "^[%l%d]+%f[^%l%d]",
+            },
+            "^().*()$",
+          },
+          g = function(ai_type)
+            local start_line, end_line = 1, vim.fn.line "$"
+            if ai_type == "i" then
+              -- skip first and last blank lines for `i` textobject
+              local first_nonblank, last_nonblank = vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
+              -- do nothing for buffer with all blanks
+              if first_nonblank == 0 or last_nonblank == 0 then return { from = { line = start_line, col = 1 } } end
+              start_line, end_line = first_nonblank, last_nonblank
+            end
+
+            local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+            return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
+          end,
+          i = function(ai_type) -- i for indent. "a" is line-wise, "i" is character-wise
+            local spaces = (" "):rep(vim.o.tabstop)
+            local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+            local indents = {}
+            for l, line in ipairs(lines) do
+              if not line:find "^%s*$" then
+                indents[#indents + 1] = { line = l, indent = #line:gsub("\t", spaces):match "^%s*", text = line }
+              end
+            end
+            local ret = {}
+            for i = 1, #indents do
+              if i == 1 or indents[i - 1].indent < indents[i].indent then
+                local from, to = i, i
+                for j = i + 1, #indents do
+                  if indents[j].indent < indents[i].indent then break end
+                  to = j
+                end
+                from = ai_type == "a" and from > 1 and from - 1 or from
+                to = ai_type == "a" and to < #indents and to + 1 or to
+                ret[#ret + 1] = {
+                  indent = indents[i].indent,
+                  from = { line = indents[from].line, col = ai_type == "a" and 1 or indents[from].indent + 1 },
+                  to = { line = indents[to].line, col = #indents[to].text },
+                }
+              end
+            end
+            return ret
+          end,
+          u = ai.gen_spec.function_call(), -- u for "Usage"
+          U = ai.gen_spec.function_call { name_pattern = "[%w_]" }, -- without dot in function name
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("mini.ai").setup(opts)
+      ds.plugin.on_load("which-key.nvim", function()
+        local mini_motions = function()
+          local i = {
+            [" "] = "mini.ai: whitespace",
+            ['"'] = 'mini.ai: balanced "',
+            ["'"] = "mini.ai: balanced '",
+            ["`"] = "mini.ai: balanced `",
+            ["("] = "mini.ai: balanced (",
+            [")"] = "mini.ai: balanced ) including white-space",
+            [">"] = "mini.ai: balanced > including white-space",
+            ["<lt>"] = "mini.ai: balanced <",
+            ["]"] = "mini.ai: balanced ] including white-space",
+            ["["] = "mini.ai: balanced [",
+            ["}"] = "mini.ai: balanced } including white-space",
+            ["{"] = "mini.ai: balanced {",
+            ["?"] = "mini.ai: user prompt",
+            _ = "mini.ai: underscore",
+            a = "mini.ai: argument",
+            b = "mini.ai: balanced ), ], }",
+            c = "mini.ai: class",
+            d = "mini.ai: digit(s)",
+            e = "mini.ai: word with (camel/snake) case",
+            f = "mini.ai: function",
+            g = "mini.ai: entire buffer",
+            o = "mini.ai: block, conditional, loop",
+            q = "mini.ai: quote `, \", '",
+            t = "mini.ai: tag",
+            u = "mini.ai: use/call function and method",
+            U = "mini.ai: use/call without dot in function name",
+          }
+          local a = vim.deepcopy(i)
+          for k, v in pairs(a) do
+            a[k] = v:gsub(" including.*", "")
+          end
+          local ic = vim.deepcopy(i)
+          local ac = vim.deepcopy(a)
+          for key, name in pairs { n = "next", p = "previous" } do
+            ---@diagnostic disable-next-line: assign-type-mismatch
+            i[key] = vim.tbl_extend("force", { name = "mini.ai: inside " .. name .. " textobject" }, ic)
+            ---@diagnostic disable-next-line: assign-type-mismatch
+            a[key] = vim.tbl_extend("force", { name = "mini.ai: around " .. name .. " textobject" }, ac)
+          end
+          require("which-key").register {
+            mode = { "o", "x" },
+            i = i,
+            a = a,
+          }
+        end
+        vim.schedule(mini_motions)
+      end)
+    end,
+  },
+  {
+    "echasnovski/mini.align",
+    keys = {
+      { "g=", desc = "mini.align: align", mode = { "v", "n" } },
+      { "g+", desc = "mini.align: align with preview", mode = { "v", "n" } },
+    },
+    opts = {
+      mappings = {
+        start = "g=",
+        start_with_preview = "g+",
+      },
+    },
+  },
+  {
+    "echasnovski/mini.hipatterns",
+    event = "LazyFile",
+    opts = function(_, opts)
+      local hi = require "mini.hipatterns"
+
+      local vtext = ds.pad(ds.icons.misc.FilledCircleLarge, "right")
+      local style = "full" -- "full" | "compact"
+      local tailwind_colors = require("ft.css").tailwind_colors
+      local filetypes = { "css", "html", "javascript", "javascriptreact", "typescript", "typescriptreact" }
+
+      local colors ---@type ColorPalette
+      local cache = {} ---@type table<string,table<string,string>>
+      local hl_groups = {} ---@type table<string,boolean>
+
+      local get_hl_group = function(hl)
+        local group = vim.inspect(hl):gsub("%W+", "_")
+        if not hl_groups[group] then
+          hl = type(hl) == "string" and { link = hl } or hl
+          hl = vim.deepcopy(hl, true)
+          hl.fg = hl.fg or vim.g.ds_colors.gray2
+          vim.api.nvim_set_hl(0, group, hl)
+          hl_groups[group] = true
+        end
+        return group
+      end
+
+      local load_highlights = function(group)
+        if cache[group] then return end
+        cache[group] = {}
+        colors = vim.g.ds_colors
+        local highlights = require("theme").defaults(colors)
+        for k, v in pairs(highlights) do
+          cache[group][k] = get_hl_group(v)
+        end
+      end
+
+      local get_file = function(buf)
+        local fname = vim.api.nvim_buf_get_name(buf or 0)
+        fname = vim.fs.normalize(fname)
+        if not fname:find "lua/theme" then return end
+        return vim.fn.fnamemodify(fname, ":t:r")
+      end
+
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        group = vim.api.nvim_create_augroup("theme_highlighter", { clear = true }),
+        pattern = "*/lua/theme/**.lua",
+        callback = vim.schedule_wrap(function(ev)
+          vim.cmd.colorscheme(vim.g.colors_name)
+          hl_groups = {}
+          local group = get_file(ev.buf)
+          if group then cache[group] = nil end
+          for _, buf in ipairs(hi.get_enabled_buffers()) do
+            hi.update(buf)
+          end
+        end),
+      })
+
+      opts.highlighters = opts.highlighters or {}
+      opts.highlighters.nvim_theme = {
+        pattern = function(buf)
+          local f = get_file(buf)
+          if not f then return end
+          load_highlights(f)
+          return f and '^%s*%[?"?()[%w%.@]+()"?%]?%s*='
+        end,
+        group = function(buf, match, _)
+          local name = get_file(buf)
+          return name and cache[name][match]
+        end,
+        extmark_opts = { priority = 2000 },
+      }
+      opts.highlighters.nvim_theme_colors = {
+        pattern = {
+          "%f[%w]()c%.[%w_%.]+()%f[%W]",
+          "%f[%w]()vim%.g%.ds_colors%.[%w_%.]+()%f[%W]",
+          "%f[%w]()vim%.g%.terminal_color_%d+()%f[%W]",
+        },
+        group = function(_, match)
+          local parts = vim.split(match, ".", { plain = true })
+          local t = _G --[[@as table]]
+          if parts[1]:sub(1, 1) == "c" then
+            table.remove(parts, 1)
+            colors = colors or vim.g.ds_colors
+            t = colors
+          end
+          local color = vim.tbl_get(t, unpack(parts))
+          return type(color) == "string" and get_hl_group { fg = color }
+        end,
+        extmark_opts = function(_, _, data)
+          return { virt_text = { { vtext, data.hl_group } }, virt_text_pos = "inline", priority = 2000 }
+        end,
+      }
+      local hex = hi.gen_highlighter.hex_color { priority = 2000, style = "inline", inline_text = vtext }
+      opts.highlighters.hex_color = {
+        pattern = function(buf)
+          local f = get_file(buf)
+          if not (f or vim.tbl_contains(filetypes, vim.bo[buf].ft)) then return end
+          return hex.pattern()
+        end,
+        group = hex.group,
+        extmark_opts = function(_, _, data)
+          return { virt_text = { { vtext, data.hl_group } }, virt_text_pos = "inline", priority = 2000 }
+        end,
+      }
+      opts.highlighters.hex_shorthand = {
+        pattern = "()#%x%x%x()%f[^%x%w]",
+        group = function(_, _, data)
+          ---@type string
+          local match = data.full_match
+          local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
+          local hex_color = "#" .. r .. r .. g .. g .. b .. b
+
+          return MiniHipatterns.compute_hex_color_group(hex_color, "fg")
+        end,
+        extmark_opts = function(_, _, data)
+          return { virt_text = { { vtext, data.hl_group } }, virt_text_pos = "inline", priority = 2000 }
+        end,
+      }
+      opts.highlighters.tailwind = {
+        pattern = function(buf)
+          if not vim.tbl_contains(filetypes, vim.bo[buf].ft) then return end
+          if style == "full" then
+            return "%f[%w:-]()[%w:-]+%-[a-z%-]+%-%d+()%f[^%w:-]"
+          elseif style == "compact" then
+            return "%f[%w:-][%w:-]+%-()[a-z%-]+%-%d+()%f[^%w:-]"
+          end
+        end,
+        group = function(_, _, data)
+          local match = data.full_match
+          local color, shade = match:match "[%w-]+%-([a-z%-]+)%-(%d+)"
+          shade = tonumber(shade)
+          local bg = vim.tbl_get(tailwind_colors, color, shade)
+          if bg then
+            local hl = "MiniHipatternsTailwind" .. color .. shade
+            vim.api.nvim_set_hl(0, hl, { fg = "#" .. bg })
+            return hl
+          end
+        end,
+        extmark_opts = function(_, _, data)
+          return { virt_text = { { vtext, data.hl_group } }, virt_text_pos = "inline", priority = 2000 }
+        end,
+      }
+    end,
+  },
+  {
+    "echasnovski/mini.pairs",
+    event = "LazyFile",
+    opts = {
+      markdown = true,
+      modes = { insert = true, command = true, terminal = false },
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      skip_ts = { "string" },
+      skip_unbalanced = true,
+    },
+    config = function(_, opts)
+      local pairs = require "mini.pairs"
+      pairs.setup(opts)
+      local open = pairs.open
+      ---@diagnostic disable-next-line: duplicate-set-field
+      pairs.open = function(pair, neigh_pattern)
+        if vim.fn.getcmdline() ~= "" then return open(pair, neigh_pattern) end
+        local _o, _c = pair:sub(1, 1), pair:sub(2, 2)
+        local line = vim.api.nvim_get_current_line()
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        local next = line:sub(cursor[2] + 1, cursor[2] + 1)
+        local before = line:sub(1, cursor[2])
+        if opts.markdown and _o == "`" and vim.bo.filetype == "markdown" and before:match "^%s*``" then
+          return "`\n```" .. vim.api.nvim_replace_termcodes("<up>", true, true, true)
+        end
+        if opts.skip_next and next ~= "" and next:match(opts.skip_next) then return _o end
+        if opts.skip_ts and #opts.skip_ts > 0 then
+          local ok, captures = pcall(vim.treesitter.get_captures_at_pos, 0, cursor[1] - 1, math.max(cursor[2] - 1, 0))
+          for _, capture in ipairs(ok and captures or {}) do
+            if vim.tbl_contains(opts.skip_ts, capture.capture) then return _o end
+          end
+        end
+        if opts.skip_unbalanced and next == _c and _c ~= _o then
+          local _, count_open = line:gsub(vim.pesc(pair:sub(1, 1)), "")
+          local _, count_close = line:gsub(vim.pesc(pair:sub(2, 2)), "")
+          if count_close > count_open then return _o end
+        end
+        return open(pair, neigh_pattern)
+      end
+      vim.api.nvim_create_user_command("PairsDisable", function(args)
+        if args.bang then
+          vim.b.minipairs_disable = true
+        else
+          vim.g.minipairs_disable = true
+        end
+        print "Disabled auto pairs"
+      end, {
+        desc = "mini.pairs: disable auto pairs",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("PairsEnable", function()
+        vim.b.minipairs_disable = false
+        vim.g.minipairs_disable = false
+        print "Enabled auto pairs"
+      end, {
+        desc = "mini.pairs: enable auto pairs",
+      })
+    end,
+  },
+  {
+    "echasnovski/mini.surround",
+    keys = function(_, keys)
+      local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+      local mappings = {
+        { opts.mappings.add, desc = "mini.surround: add surrounding", mode = { "n", "v" } },
+        { opts.mappings.delete, desc = "mini.surround: delete surrounding" },
+        { opts.mappings.find, desc = "mini.surround: find right surrounding" },
+        { opts.mappings.find_left, desc = "mini.surround: find left surrounding" },
+        { opts.mappings.highlight, desc = "mini.surround: highlight surrounding" },
+        { opts.mappings.replace, desc = "mini.surround: replace surrounding" },
+        { opts.mappings.update_n_lines, desc = "mini.surround: update `MiniSurround.config.n_lines`" },
+      }
+      mappings = vim.tbl_filter(function(m) return m[1] and #m[1] > 0 end, mappings)
+      return vim.list_extend(mappings, keys)
+    end,
+    opts = {
+      mappings = {
+        add = "ysa",
+        delete = "ysd",
+        find = "ysf",
+        find_left = "ysF",
+        highlight = "ysh",
+        replace = "ysr",
+        update_n_lines = "ysn",
+      },
+    },
+  },
+  {
+    "echasnovski/mini.splitjoin",
+    keys = {
+      { "gJ", desc = "mini.splitjoin: split arguments" },
+      { "gj", desc = "mini.splitjoin: join arguments" },
+    },
+    opts = {
+      mappings = {
+        toggle = "",
+        split = "gJ",
+        join = "gj",
+      },
+    },
+    config = function(_, opts) require("mini.splitjoin").setup(opts) end,
+  },
+}
