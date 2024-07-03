64310554361a386b909148996c5c1cda40388b3d
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-07-03 15:50:46 -0500

    chore(nvim): whitespace formatting

--- nvim/lua/theme/palette/catppuccin/mocha.lua
+++ nvim/lua/theme/palette/catppuccin/mocha.lua
@@ -1,55 +1,56 @@
+-- stylua: ignore
 ---@type ColorPalette
 ---@diagnostic disable-next-line: missing-fields
 local c = {
-  bg0 = "#11111b", --crust
-  bg1 = "#181825", --mantle
-  bg2 = "#1e1e2e", --base
-  bg3 = "#24273a",
-  bg4 = "#303446",
+  bg0      = "#11111b", --crust
+  bg1      = "#181825", --mantle
+  bg2      = "#1e1e2e", --base
+  bg3      = "#24273a",
+  bg4      = "#303446",
 
-  fg0 = "#a6adc8", --subtext0
-  fg1 = "#bac2de", --subtext1
-  fg2 = "#cdd6f4", --text
+  fg0      = "#a6adc8", --subtext0
+  fg1      = "#bac2de", --subtext1
+  fg2      = "#cdd6f4", --text
 
-  gray0 = "#313244", --surface2
-  gray1 = "#45475a", --surface1
-  gray2 = "#585b70", --surface0
+  gray0    = "#313244", --surface2
+  gray1    = "#45475a", --surface1
+  gray2    = "#585b70", --surface0
 
-  rose0 = "#f2cdcd", --flamingo
-  rose1 = "#f5e0dc", --rosewater
+  rose0    = "#f2cdcd", --flamingo
+  rose1    = "#f5e0dc", --rosewater
 
-  red0 = "#eba0ac", --maroon
-  red1 = "#f38ba8", --red
-  red2 = "#c06d84",
-  red3 = "#8d5060",
+  red0     = "#eba0ac", --maroon
+  red1     = "#f38ba8", --red
+  red2     = "#c06d84",
+  red3     = "#8d5060",
 
-  orange0 = "#fab387", --peach
-  orange1 = "#ffab87",
+  orange0  = "#fab387", --peach
+  orange1  = "#ffab87",
 
-  yellow0 = "#e4c78f",
-  yellow1 = "#f9e2af", --yellow
-  yellow2 = "#ffecb3",
+  yellow0  = "#e4c78f",
+  yellow1  = "#f9e2af", --yellow
+  yellow2  = "#ffecb3",
 
-  green0 = "#a6d189",
-  green1 = "#a6e3a1", --green
-  green2 = "#b0f8af",
+  green0   = "#a6d189",
+  green1   = "#a6e3a1", --green
+  green2   = "#b0f8af",
 
-  cyan0 = "#8bd5ca",
-  cyan1 = "#94e2d5", --teal
-  cyan2 = "#a0e2d7",
+  cyan0    = "#8bd5ca",
+  cyan1    = "#94e2d5", --teal
+  cyan2    = "#a0e2d7",
 
-  aqua0 = "#74c7ec", --sapphire
-  aqua1 = "#89dceb", --sky
-  aqua2 = "#94e7ff",
+  aqua0    = "#74c7ec", --sapphire
+  aqua1    = "#89dceb", --sky
+  aqua2    = "#94e7ff",
 
-  blue0 = "#6d8fc6",
-  blue1 = "#89b4fa", --blue
-  blue2 = "#93b4ff",
-  blue3 = "#afc1f1",
-  blue4 = "#b3c9f6",
+  blue0    = "#6d8fc6",
+  blue1    = "#89b4fa", --blue
+  blue2    = "#93b4ff",
+  blue3    = "#afc1f1",
+  blue4    = "#b3c9f6",
 
-  purple0 = "#b4befe", --lavender
-  purple1 = "#cba6f7", --mauve
+  purple0  = "#b4befe", --lavender
+  purple1  = "#cba6f7", --mauve
 
   magenta0 = "#c299b6",
   magenta1 = "#ddb9d7",
@@ -59,20 +60,22 @@ local c = {
   overlay1 = "#7f849c",
 }
 
-c.black = ds.color.darken(c.bg2, 50)
-c.white = ds.color.darken(c.fg1, 10)
+-- stylua: ignore start
+c.black       = ds.color.darken(c.bg2, 50)
+c.white       = ds.color.darken(c.fg1, 10)
 
-c.bgX = ds.color.blend(c.bg0, c.bg2, 0.31)
-c.bg_visual = "#585b70"
+c.bgX         = ds.color.blend(c.bg0, c.bg2, 0.31)
+c.bg_visual   = "#585b70"
 
-c.fg_conceal = ds.color.blend(c.magenta2, c.bg2, 0.44)
-c.fg_comment = "#9399b2"
+c.fg_conceal  = ds.color.blend(c.magenta2, c.bg2, 0.44)
+c.fg_comment  = "#9399b2"
 
-c.grayX = ds.color.darken(c.gray1, 25)
+c.grayX       = ds.color.darken(c.gray1, 25)
 
-c.diff_add = "#a5e2a0"
+c.diff_add    = "#a5e2a0"
 c.diff_delete = "#f38ba8"
-c.diff_text = "#87b2f9"
+c.diff_text   = "#87b2f9"
 c.diff_change = ds.color.blend(c.diff_text, c.bg3, 0.2)
+-- stylua: ignore end
 
 return c
--- nvim/lua/theme/palette/kdark.lua
+++ nvim/lua/theme/palette/kdark.lua
@@ -1,55 +1,56 @@
+-- stylua: ignore
 ---@type ColorPalette
 ---@diagnostic disable-next-line: missing-fields
 local c = {
-  bg0 = "#1f2021",
-  bg1 = "#323233",
-  bg2 = "#373737",
-  bg3 = "#3d3e40",
-  bg4 = "#3a3c3d",
+  bg0      = "#1f2021",
+  bg1      = "#323233",
+  bg2      = "#373737",
+  bg3      = "#3d3e40",
+  bg4      = "#3a3c3d",
 
-  fg0 = "#b8bec9",
-  fg1 = "#c9d1d9",
-  fg2 = "#d8dee9",
+  fg0      = "#b8bec9",
+  fg1      = "#c9d1d9",
+  fg2      = "#d8dee9",
 
-  gray0 = "#414145",
-  gray1 = "#59595e",
-  gray2 = "#97979f",
+  gray0    = "#414145",
+  gray1    = "#59595e",
+  gray2    = "#97979f",
 
-  rose0 = "#cd8d88",
-  rose1 = "#e4c1be",
+  rose0    = "#cd8d88",
+  rose1    = "#e4c1be",
 
-  red0 = "#a76369",
-  red1 = "#bf616a",
-  red2 = "#e5747f",
-  red3 = "#f7768e",
+  red0     = "#a76369",
+  red1     = "#bf616a",
+  red2     = "#e5747f",
+  red3     = "#f7768e",
 
-  orange0 = "#d08770",
-  orange1 = "#ffab87",
+  orange0  = "#d08770",
+  orange1  = "#ffab87",
 
-  yellow0 = "#d3b67d",
-  yellow1 = "#e0af68",
-  yellow2 = "#ebcb8b",
+  yellow0  = "#d3b67d",
+  yellow1  = "#e0af68",
+  yellow2  = "#ebcb8b",
 
-  green0 = "#7f966d",
-  green1 = "#97b182",
-  green2 = "#a3be8c",
+  green0   = "#7f966d",
+  green1   = "#97b182",
+  green2   = "#a3be8c",
 
-  cyan0 = "#5a8d83",
-  cyan1 = "#79bdaf",
-  cyan2 = "#8fbcbb",
+  cyan0    = "#5a8d83",
+  cyan1    = "#79bdaf",
+  cyan2    = "#8fbcbb",
 
-  aqua0 = "#5f8f9d",
-  aqua1 = "#77b3c5",
-  aqua2 = "#8ed6ec",
+  aqua0    = "#5f8f9d",
+  aqua1    = "#77b3c5",
+  aqua2    = "#8ed6ec",
 
-  blue0 = "#516882",
-  blue1 = "#6f8fb4",
-  blue2 = "#81a1c1",
-  blue3 = "#90b4d9",
-  blue4 = "#97b6e5",
+  blue0    = "#516882",
+  blue1    = "#6f8fb4",
+  blue2    = "#81a1c1",
+  blue3    = "#90b4d9",
+  blue4    = "#97b6e5",
 
-  purple0 = "#94628a",
-  purple1 = "#ac72a0",
+  purple0  = "#94628a",
+  purple1  = "#ac72a0",
 
   magenta0 = "#90718a",
   magenta1 = "#b48ead",
@@ -58,21 +59,22 @@ local c = {
   overlay0 = "#6c7086",
   overlay1 = "#7f849c",
 }
+-- stylua: ignore start
+c.black       = ds.color.darken(c.bg2, 50)
+c.white       = ds.color.darken(c.fg1, 10)
 
-c.black = ds.color.darken(c.bg2, 50)
-c.white = ds.color.darken(c.fg1, 10)
+c.bgX         = ds.color.blend(c.bg0, c.bg2, 0.31)
+c.bg_visual   = ds.color.blend(c.blue1, c.bg2, 0.31)
 
-c.bgX = ds.color.blend(c.bg0, c.bg2, 0.31)
-c.bg_visual = ds.color.blend(c.blue1, c.bg2, 0.31)
+c.fg_conceal  = ds.color.blend(c.magenta2, c.bg2, 0.44)
+c.fg_comment  = ds.color.blend(c.blue0, c.gray1, 0.31)
 
-c.fg_conceal = ds.color.blend(c.magenta2, c.bg2, 0.44)
-c.fg_comment = ds.color.blend(c.blue0, c.gray1, 0.31)
+c.grayX       = ds.color.darken(c.gray1, 25)
 
-c.grayX = ds.color.darken(c.gray1, 25)
-
-c.diff_add = "#132e1f"
+c.diff_add    = "#132e1f"
 c.diff_delete = "#361f21"
-c.diff_text = "#1b3956"
+c.diff_text   = "#1b3956"
 c.diff_change = ds.color.blend(c.diff_text, c.bg3, 0.2)
+-- stylua: ignore end
 
 return c
