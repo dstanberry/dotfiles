7a34c1080224e6181f3b968fcd374df9c3b34e06
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-07-05 21:38:17 -0500

    feat(nvim): test-drive `mini.files`

--- nvim/lua/remote/mini.lua
+++ nvim/lua/remote/mini.lua
@@ -139,6 +139,120 @@ return {
     },
   },
   {
+    "echasnovski/mini.files",
+    keys = function()
+      local _cwd = function() require("mini.files").open(vim.api.nvim_buf_get_name(0), true) end
+      return {
+        { "<leader>fm", _cwd, desc = "mini.files: open current working directory" },
+      }
+    end,
+    opts = {
+      options = { use_as_default_explorer = false },
+      windows = { preview = false },
+      mappings = {
+        change_cwd = "gc",
+        close = "q",
+        go_in = "l",
+        go_out = "h",
+        go_in_plus = "<cr>",
+        go_out_plus = "",
+        go_in_vertical = "<c-v>",
+        reset = "<BS>",
+        reveal_cwd = "@",
+        show_help = "g?",
+        synchronize = "=",
+        toggle_hidden = "g.",
+        trim_left = "<",
+        trim_right = ">",
+      },
+    },
+    init = function()
+      ds.hl.new("MiniFilesBorder", { link = "FloatBorderSB" })
+      ds.hl.new("MiniFilesBorderModified", { fg = vim.g.ds_colors.rose0, bg = vim.g.ds_colors.bg0 })
+      ds.hl.new("MiniFilesCursorLine", { bg = vim.g.ds_colors.bg0 })
+      ds.hl.new("MiniFilesTitle", { fg = vim.g.ds_colors.gray2, bg = vim.g.ds_colors.bg0 })
+      ds.hl.new("MiniFilesTitleFocused", { fg = vim.g.ds_colors.blue1, bg = vim.g.ds_colors.bg0 })
+    end,
+    config = function(_, opts)
+      require("mini.files").setup(opts)
+
+      local filter_show = function() return true end
+      local filter_hide = function(fs_entry) return not vim.startswith(fs_entry.name, ".") end
+      local show_dotfiles = true
+
+      local toggle_dotfiles = function()
+        show_dotfiles = not show_dotfiles
+        local new_filter = show_dotfiles and filter_show or filter_hide
+        require("mini.files").refresh { content = { filter = new_filter } }
+      end
+
+      local map_split = function(buf_id, lhs, direction, close_on_file)
+        local rhs = function()
+          local new_target_window
+          local cur_target_window = require("mini.files").get_target_window()
+          if cur_target_window ~= nil then
+            vim.api.nvim_win_call(cur_target_window, function()
+              vim.cmd("belowright " .. direction .. " split")
+              new_target_window = vim.api.nvim_get_current_win()
+            end)
+
+            require("mini.files").set_target_window(new_target_window)
+            require("mini.files").go_in { close_on_file = close_on_file }
+          end
+        end
+
+        local desc = "mini.files: open in " .. direction .. " split"
+        if close_on_file then desc = desc .. " and close" end
+        vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+      end
+
+      local files_set_cwd = function()
+        local cur_entry_path = MiniFiles.get_fs_entry().path
+        local cur_directory = vim.fs.dirname(cur_entry_path)
+        if cur_directory ~= nil then vim.fn.chdir(cur_directory) end
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesWindowOpen",
+        callback = function(args)
+          local win_id = args.data.win_id
+
+          local config = vim.api.nvim_win_get_config(win_id)
+          config.border = ds.map(ds.icons.border.Default, function(icon) return { icon, "FloatBorderSB" } end)
+          config.title_pos = "center"
+          vim.api.nvim_win_set_config(win_id, config)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          local buf_id = args.data.buf_id
+
+          vim.keymap.set(
+            "n",
+            opts.mappings.toggle_hidden,
+            toggle_dotfiles,
+            { buffer = buf_id, desc = "mini.files: toggle hidden" }
+          )
+          vim.keymap.set(
+            "n",
+            opts.mappings.change_cwd,
+            files_set_cwd,
+            { buffer = args.data.buf_id, desc = "mini.files: set cwd" }
+          )
+
+          map_split(buf_id, opts.mappings.go_in_vertical, "vertical", true)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesActionRename",
+        callback = function(event) require("remote.lsp.handlers").on_rename(event.data.from, event.data.to) end,
+      })
+    end,
+  },
+  {
     "echasnovski/mini.hipatterns",
     event = "LazyFile",
     opts = function(_, opts)
