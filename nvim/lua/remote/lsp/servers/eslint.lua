ded70c754f0d82fd3884f2135596bcd3fd0fae5a
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-07-08 16:29:51 -0500

    fix(nvim): eslint configuration

--- nvim/lua/remote/lsp/servers/eslint.lua
+++ nvim/lua/remote/lsp/servers/eslint.lua
@@ -2,12 +2,18 @@ local M = {}
 
 M.config = {
   settings = {
-    workingDirectory = { mode = "auto" },
+    workingDirectories = { mode = "auto" },
   },
   on_attach = function(_, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
-      command = "EslintFixAll",
+      callback = function(args)
+        local client = vim.lsp.get_clients({ bufnr = args.buf, name = "eslint" })[1]
+        if client then
+          local diag = vim.diagnostic.get(args.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+          if #diag > 0 then vim.cmd "EslintFixAll" end
+        end
+      end,
     })
   end,
 }
