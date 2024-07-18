f33fcc06ac8e592f7f8b0cab73fae9d5f80e45ab
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-07-18 10:19:06 -0500

    chore(nvim): remove comment

--- nvim/queries/python/highlights.scm
+++ nvim/queries/python/highlights.scm
@@ -7,5 +7,4 @@
 ((identifier) @constant
  (#lua-match? @constant "^[A-Z][A-Z_0-9]*$"))
 
-; comment corresponding line in stdpath("data")/lazy/nvim-treesitter/queries/python/highlights.scm
 ((string) @string (#set! "priority" 95))
