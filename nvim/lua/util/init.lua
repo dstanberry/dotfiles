ed71e3b9243105cd9bc78548e75e4e29831e2646
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-07-18 13:35:01 -0500

    fix(nvim): regex matcher

--- nvim/lua/util/init.lua
+++ nvim/lua/util/init.lua
@@ -116,7 +116,7 @@ end
 function M.get_module(filepath)
   local mod, sep
   sep = ds.has "win32" and "\\" or "/"
-  mod = filepath:match(("lua%s(.+)%.lua$"):format(sep)):gsub(sep, "."):gsub("%.init", "")
+  mod = filepath:match("lua%S(.+)%.lua$"):format(sep):gsub(sep, "."):gsub("%.init", "")
   return mod or ""
 end
 
