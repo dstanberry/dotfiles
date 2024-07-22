fbe6b20d7d7b14b6cb1addd569656fa904684fe0
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-07-22 09:35:50 -0500

    fix(nvim): lsp root_dir matcher

--- nvim/lua/remote/lsp/servers/marksman.lua
+++ nvim/lua/remote/lsp/servers/marksman.lua
@@ -2,13 +2,9 @@ local M = {}
 
 M.config = {
   root_dir = function(fname)
-    local ok, lsp_util = pcall(require, "lspconfig.util")
-    local root_dirs = { ".marksman.toml", ".zk" }
-    return ok
-        and (lsp_util.find_git_ancestor(fname) or lsp_util.root_pattern(unpack(root_dirs))(fname) or lsp_util.path.dirname(
-          fname
-        ))
-      or ds.buffer.get_root()
+    return require("lspconfig.util").root_pattern(".marksman.toml", ".zk")(fname)
+      or require("lspconfig.util").find_git_ancestor(fname)
+      or vim.uv.cwd()
   end,
 }
 
