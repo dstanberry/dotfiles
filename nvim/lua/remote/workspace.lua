8f879e71d21f8513094715e85b9c467a2048a829
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-07-23 09:03:04 -0500

    chore(nvim): update `grug-far` configuration

--- nvim/lua/remote/workspace.lua
+++ nvim/lua/remote/workspace.lua
@@ -280,6 +280,7 @@ return {
         local grug = require "grug-far"
         local ext = vim.bo.buftype == "" and vim.fn.expand "%:e"
         grug.grug_far {
+          transient = true,
           prefills = {
             filesFilter = ext and ext ~= "" and "*." .. ext or nil,
           },
