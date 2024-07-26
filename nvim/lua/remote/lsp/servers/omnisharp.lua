5d8134497918c9045a7ec3dc143cd1e04edea6d8
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-07-25 22:36:44 -0500

    feat(nvim): organize usings on save for csharp

--- nvim/lua/remote/lsp/servers/omnisharp.lua
+++ nvim/lua/remote/lsp/servers/omnisharp.lua
@@ -21,6 +21,7 @@ M.config = {
   },
   on_attach = function(client, bufnr)
     require("remote.lsp.handlers").on_attach(client, bufnr)
+
     local opts = {
       reuse_win = true,
       layout_strategy = "vertical",
@@ -31,14 +32,57 @@ M.config = {
       layout_config = { height = 40, prompt_position = "bottom" },
       path_display = { "shorten" },
     }
+
     local _definition = function() require("omnisharp_extended").telescope_lsp_definition(opts) end
     local _implementation = function() require("omnisharp_extended").telescope_lsp_implementation(opts) end
     local _references = function() require("omnisharp_extended").telescope_lsp_references(ropts) end
     local _type_definition = function() require("omnisharp_extended").telescope_lsp_type_definition(opts) end
+
     vim.keymap.set("n", "gd", _definition, { buffer = bufnr, desc = "omnisharp: goto definition" })
     vim.keymap.set("n", "gi", _implementation, { buffer = bufnr, desc = "omnisharp: goto implementation" })
     vim.keymap.set("n", "gr", _references, { buffer = bufnr, desc = "omnisharp: show references" })
     vim.keymap.set("n", "gt", _type_definition, { buffer = bufnr, desc = "omnisharp: goto type definition" })
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = ds.augroup "lsp_omnisharp",
+      pattern = "*.cs",
+      callback = function()
+        if not (client and bufnr) then return end
+        local params = vim.lsp.util.make_position_params(0, "utf-8")
+        local request = {
+          Column = params.position.character,
+          Line = params.position.line,
+          FileName = vim.uri_to_fname(params.textDocument.uri),
+          WantsTextChanges = true,
+          ApplyTextChanges = false,
+        }
+        local response = client.request_sync("o#/fixusings", request, 500, bufnr)
+        if response.err then
+          ds.error(
+            "`fixusings` failed:\n" .. response.err,
+            { title = "Lsp: omnisharp", lang = "markdown", merge = true }
+          )
+        elseif response.result.Changes then
+          local edits = {}
+          for _, change in pairs(response.result.Changes) do
+            table.insert(edits, {
+              newText = change.NewText,
+              range = {
+                start = {
+                  line = change.StartLine,
+                  character = change.StartColumn,
+                },
+                ["end"] = {
+                  line = change.EndLine,
+                  character = change.EndColumn,
+                },
+              },
+            })
+          end
+          vim.lsp.util.apply_text_edits(edits, bufnr, "utf-8")
+        end
+      end,
+    })
   end,
 }
 
