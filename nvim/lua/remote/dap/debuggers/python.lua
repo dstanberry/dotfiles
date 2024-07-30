7558d278f4ef74396a996ad78f59a16b4c631dbe
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-07-30 15:41:13 -0500

    fix(nvim): `dap-python` configuration

--- nvim/lua/remote/dap/debuggers/python.lua
+++ nvim/lua/remote/dap/debuggers/python.lua
@@ -1,18 +1,25 @@
 local M = {}
 
 local initialize = function(bufnr)
-  local filepath = ds.has "win32" and ds.plugin.get_pkg_path("debugpy", "venv/Scripts/pythonw.exe")
-    or ds.plugin.get_pkg_path("debugpy", "venv/bin/python")
+  if not M.initialized then
+    M.initialized = true
 
-  local _method = function() require("dap-python").test_method() end
-  local _class = function() require("dap-python").test_class() end
+    local py = require "dap-python"
+    local filepath = ds.has "win32" and ds.plugin.get_pkg_path("debugpy", "venv/Scripts/pythonw.exe")
+      or ds.plugin.get_pkg_path("debugpy", "venv/bin/python")
 
-  vim.keymap.set("n", "<leader>dp", "", { buffer = bufnr, desc = "+dap: python" })
-  vim.keymap.set("n", "<leader>dpm", _method, { buffer = bufnr, desc = "python: debug method" })
-  vim.keymap.set("n", "<leader>dpc", _class, { buffer = bufnr, desc = "python: debug class" })
+    local _method = function() py.test_method() end
+    local _class = function() py.test_class() end
 
-  require("dap-python").setup(filepath)
+    py.setup(filepath, { console = "integratedTerminal", include_configs = true, pythonPath = filepath })
+
+    vim.keymap.set("n", "<leader>dp", "", { buffer = bufnr, desc = "+dap: python" })
+    vim.keymap.set("n", "<leader>dpm", _method, { buffer = bufnr, desc = "python: debug method" })
+    vim.keymap.set("n", "<leader>dpc", _class, { buffer = bufnr, desc = "python: debug class" })
+  end
 end
+
+M.initialized = false
 
 M.setup = function()
   local bufnr = vim.api.nvim_get_current_buf()
@@ -20,9 +27,10 @@ M.setup = function()
     vim.api.nvim_create_autocmd("FileType", {
       group = ds.augroup "dap_python",
       pattern = "python",
+      once = true,
       callback = function(args) initialize(args.buf) end,
     })
-  else
+  elseif not M.initialized then
     initialize(bufnr)
   end
 end
