72670ca17c2b592cf2d587b1dd4f25230bcaa09b
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-09-09 09:31:55 -0500

    fix(wezterm): fix keymaps

--- wezterm/keys.lua
+++ wezterm/keys.lua
@@ -4,27 +4,25 @@ local wezterm = require "wezterm" --[[@as wezterm]]
 local M = {}
 
 M.primary_mod = "SHIFT|CTRL"
-M.alternate_mod = "ALT|CTRL"
-
-local action = wezterm.action
+M.alternate_mod = "SHIFT|ALT"
 
 ---@param pane PaneObj
 function M.is_nvim(pane)
   return pane:get_user_vars().IS_NVIM == "true" or pane:get_foreground_process_name():find "n?vim"
 end
 
----@param resize_or_move "resize" | "move"
+---@param action "resize" | "move"
 ---@param mods string
 ---@param key string
 ---@param direction "Right" | "Left" | "Up" | "Down"
 ---@param ignore_process? boolean
-function M.split_or_resize(resize_or_move, mods, key, direction, ignore_process)
-  local event = "SplitNav_" .. resize_or_move .. "_" .. direction
+function M.move_or_resize(action, mods, key, direction, ignore_process)
+  local event = "SplitNav_" .. action .. "_" .. direction
   wezterm.on(event, function(win, pane)
     if M.is_nvim(pane) and not ignore_process then
       win:perform_action({ SendKey = { key = key, mods = mods } }, pane)
     else
-      if resize_or_move == "resize" then
+      if action == "resize" then
         win:perform_action({ AdjustPaneSize = { direction, 3 } }, pane)
       else
         local panes = pane:tab():panes_with_info()
@@ -54,51 +52,49 @@ function M.setup(config)
   config.disable_default_key_bindings = true
   config.keys = {
     -- font
-    { mods = M.primary_mod, key = "(", action = action.DecreaseFontSize },
-    { mods = M.primary_mod, key = ")", action = action.IncreaseFontSize },
+    { mods = M.primary_mod, key = "(", action = wezterm.action.DecreaseFontSize },
+    { mods = M.primary_mod, key = ")", action = wezterm.action.IncreaseFontSize },
     -- scrollback
-    { mods = M.primary_mod, key = "d", action = action.ScrollByPage(0.5) },
-    { mods = M.primary_mod, key = "f", action = action.ScrollByPage(-0.5) },
-    { mods = M.primary_mod, key = "t", action = action.SpawnTab "CurrentPaneDomain" },
+    { mods = M.primary_mod, key = "d", action = wezterm.action.ScrollByPage(0.5) },
+    { mods = M.primary_mod, key = "f", action = wezterm.action.ScrollByPage(-0.5) },
+    { mods = M.primary_mod, key = "t", action = wezterm.action.SpawnTab "CurrentPaneDomain" },
     -- clipboard
-    { mods = M.primary_mod, key = "c", action = action.CopyTo "Clipboard" },
-    { mods = M.primary_mod, key = "v", action = action.PasteFrom "Clipboard" },
+    { mods = M.primary_mod, key = "c", action = wezterm.action.CopyTo "Clipboard" },
+    { mods = M.primary_mod, key = "v", action = wezterm.action.PasteFrom "Clipboard" },
     -- panes
-    { mods = M.primary_mod, key = "-", action = action.SplitHorizontal { domain = "CurrentPaneDomain" } },
-    { mods = M.primary_mod, key = "/", action = action.SplitVertical { domain = "CurrentPaneDomain" } },
-    { mods = M.primary_mod, key = "d", action = action.ActivatePaneDirection "Prev" },
-    { mods = M.primary_mod, key = "f", action = action.ActivatePaneDirection "Next" },
-    { mods = M.primary_mod, key = "w", action = action.CloseCurrentPane { confirm = false } },
-    { mods = M.primary_mod, key = "z", action = action.TogglePaneZoomState },
-    M.split_or_resize("move", M.primary_mod, "h", "Left"),
-    M.split_or_resize("move", M.primary_mod, "j", "Down"),
-    M.split_or_resize("move", M.primary_mod, "k", "Up"),
-    M.split_or_resize("move", M.primary_mod, "l", "Right"),
+    { mods = M.primary_mod, key = "-", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+    { mods = M.primary_mod, key = "/", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+    { mods = M.primary_mod, key = "w", action = wezterm.action.CloseCurrentPane { confirm = false } },
+    { mods = M.primary_mod, key = "z", action = wezterm.action.TogglePaneZoomState },
+    M.move_or_resize("move", M.primary_mod, "h", "Left"),
+    M.move_or_resize("move", M.primary_mod, "j", "Down"),
+    M.move_or_resize("move", M.primary_mod, "k", "Up"),
+    M.move_or_resize("move", M.primary_mod, "l", "Right"),
     -- TODO: investigate how to make this work
-    -- M.split_or_resize("move", "CTRL", "h", "Left"),
-    -- M.split_or_resize("move", "CTRL", "j", "Down"),
-    -- M.split_or_resize("move", "CTRL", "k", "Up"),
-    -- M.split_or_resize("move", "CTRL", "l", "Right"),
-    M.split_or_resize("resize", M.alternate_mod, "h", "Right"),
-    M.split_or_resize("resize", M.alternate_mod, "j", "Down"),
-    M.split_or_resize("resize", M.alternate_mod, "k", "Up"),
-    M.split_or_resize("resize", M.alternate_mod, "l", "Left"),
-    M.split_or_resize("resize", "ALT", "h", "Right"),
-    M.split_or_resize("resize", "ALT", "j", "Down"),
-    M.split_or_resize("resize", "ALT", "k", "Up"),
-    M.split_or_resize("resize", "ALT", "l", "Left"),
+    -- M.move_or_resize("move", "CTRL", "h", "Left"),
+    -- M.move_or_resize("move", "CTRL", "j", "Down"),
+    -- M.move_or_resize("move", "CTRL", "k", "Up"),
+    -- M.move_or_resize("move", "CTRL", "l", "Right"),
+    M.move_or_resize("resize", M.alternate_mod, "h", "Right"),
+    M.move_or_resize("resize", M.alternate_mod, "j", "Down"),
+    M.move_or_resize("resize", M.alternate_mod, "k", "Up"),
+    M.move_or_resize("resize", M.alternate_mod, "l", "Left"),
+    M.move_or_resize("resize", "ALT", "h", "Right"),
+    M.move_or_resize("resize", "ALT", "j", "Down"),
+    M.move_or_resize("resize", "ALT", "k", "Up"),
+    M.move_or_resize("resize", "ALT", "l", "Left"),
     -- misc
-    { mods = M.primary_mod, key = "p", action = action.ActivateCommandPalette },
-    { mods = M.primary_mod, key = "`", action = action.ShowDebugOverlay },
+    { mods = M.primary_mod, key = "`", action = wezterm.action.ShowDebugOverlay },
+    { mods = M.primary_mod, key = "p", action = wezterm.action.ActivateCommandPalette },
   }
   -- tabs
   if wezterm.target_triple:find "windows" then
     table.insert(
       config.keys,
-      { mods = "SHIFT|CTRL", key = "1", action = wezterm.action.SpawnTab { DomainName = config.default_domain } }
+      { mods = M.primary_mod, key = "1", action = wezterm.action.SpawnTab { DomainName = config.default_domain } }
     )
     table.insert(config.keys, {
-      mods = "SHIFT|CTRL",
+      mods = M.primary_mod,
       key = "2",
       action = wezterm.action.SpawnCommandInNewTab {
         domain = { DomainName = "local" },
@@ -106,6 +102,8 @@ function M.setup(config)
       },
     })
   end
+  table.insert(config.keys, { mods = M.primary_mod, key = "e", action = wezterm.action.ActivateTabRelative(-1) })
+  table.insert(config.keys, { mods = M.primary_mod, key = "r", action = wezterm.action.ActivateTabRelative(1) })
 end
 
 return M
