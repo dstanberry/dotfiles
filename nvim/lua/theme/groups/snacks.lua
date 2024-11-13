cb4c6ab22bd5d9fbe61c7a0d7bd5ea46a5695917
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-11-13 09:47:52 -0600

    fix(nvim): highlight groups

--- nvim/lua/theme/groups/snacks.lua
+++ nvim/lua/theme/groups/snacks.lua
@@ -3,10 +3,9 @@ local M = {}
 ---@param c util.theme.palette
 ---@return util.theme.hl
 M.get = function(c)
-  -- |mini.align| `=<bs>fn==1<cr>t` 
   -- stylua: ignore
   return {
-    SnacksNormal              = { fg = c.   white, bg = c.bg0 },
+    SnacksNormal              = { fg = c.white, bg = c.bg0 },
 
     SnacksNotifierTrace       = { fg   = c.white, bg = c.bg0 },
     SnacksNotifierBorderTrace = { fg   = c.bg0, bg   = c.bg0 },
@@ -18,12 +17,12 @@ M.get = function(c)
     SnacksNotifierIconDebug   = { link = "DiagnosticHint" },
     SnacksNotifierTitleDebug  = { fg = ds.color.get_color "DiagnosticHint", bg = c.bg0 },
 
-    SnacksNotifierInfo        = { fg   = c. white, bg = c.bg0 },
+    SnacksNotifierInfo        = { fg   = c.white, bg = c.bg0 },
     SnacksNotifierBorderInfo  = { fg   = c.bg0, bg   = c.bg0 },
     SnacksNotifierIconInfo    = { link = "DiagnosticInfo" },
     SnacksNotifierTitleInfo   = { fg = ds.color.get_color "DiagnosticInfo", bg = c.bg0 },
 
-    SnacksNotifierWarn        = { fg   = c. white, bg = c.bg0 },
+    SnacksNotifierWarn        = { fg   = c.white, bg = c.bg0 },
     SnacksNotifierBorderWarn  = { fg   = c.bg0, bg   = c.bg0 },
     SnacksNotifierIconWarn    = { link = "DiagnosticWarn" },
     SnacksNotifierTitleWarn   = { fg = ds.color.get_color "DiagnosticWarn", bg = c.bg0 },
