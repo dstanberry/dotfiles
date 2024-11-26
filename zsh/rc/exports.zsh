f32e91bd81224b65d1bfc7a898bb10b5196945fc
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-11-26 13:26:38 -0600

    style(eza): add `kdark` theme

--- zsh/rc/exports.zsh
+++ zsh/rc/exports.zsh
@@ -75,6 +75,11 @@ export PROJECTS_DIR="${HOME}/Projects"
 # define the default manpager
 export MANPAGER='nvim +Man!'
 
+# define location of eza config
+if hash eza 2> /dev/null; then
+	export EZA_CONFIG_DIR="${CONFIG_HOME}/eza"
+fi
+
 # (no direct export) load angular completions when available
 if hash ng 2> /dev/null; then
   _evalcache ng completion script
