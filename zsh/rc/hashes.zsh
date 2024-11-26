c05041a6052fe2bb16b2c4a426f2b35acce216bb
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-11-26 11:34:48 -0600

    chore(zsh): update directory hashes

--- zsh/rc/hashes.zsh
+++ zsh/rc/hashes.zsh
@@ -1,5 +1,13 @@
 # shellcheck disable=SC2148
 
 # define static named directories
+hash -d docs=~/Documents
 hash -d notes=~/Documents/_notes
+hash -d dl=~/Downloads
+hash -d git=~/Git
 hash -d projects=~/Projects
+
+hash -d cache=~/.cache
+hash -d bin=~/.local/bin
+hash -d share=~/.local/share
+hash -d state=~/.local/state
