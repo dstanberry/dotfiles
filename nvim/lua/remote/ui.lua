fa312d500176a0ded46c9e3ece85ac1ff4dff983
Demaro Stanberry <demaro.stanberry@gmail.com>
2024-12-11 14:30:03 -0600

    chore(nvim): remove unused plugin

--- nvim/lua/remote/ui.lua
+++ nvim/lua/remote/ui.lua
@@ -162,31 +162,6 @@ return {
     cmd = "IncRename",
     opts = { hl_group = "Substitute" },
   },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "LazyFile",
-    opts = {
-      indent = { char = ds.icons.misc.VerticalBarMiddle },
-      scope = { enabled = false },
-      exclude = {
-        filetypes = vim.tbl_deep_extend(
-          "keep",
-          ds.excludes.ft.stl_disabled,
-          ds.excludes.ft.wb_disabled,
-          ds.excludes.ft.wb_empty,
-          { "checkhealth", "diff", "git" },
-          { "log", "markdown", "txt" }
-        ),
-      },
-    },
-    config = function(_, opts)
-      require("ibl").setup(opts)
-      local hooks = require "ibl.hooks"
-      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
-    end,
-  },
   { "MunifTanjim/nui.nvim", lazy = true },
   {
     "folke/noice.nvim",
