9ed0a010c07e0717883e2abb25ec0e4c351e91e8
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-01-08 15:14:47 -0600

    chore(nvim): change icon

--- nvim/lua/remote/snacks.lua
+++ nvim/lua/remote/snacks.lua
@@ -87,7 +87,7 @@ return {
             { key = "g", action = require("remote.telescope.util").grep.dynamic, icon = ds.pad(ds.icons.misc.Data, "right"), desc = " Find Text" },
             { key = "r", action = function() require("persistence").load() end, icon = ds.pad(ds.icons.misc.Revolve, "right"), desc = " Restore Session" },
             { key = "c", action = require("remote.telescope.util").files.nvim_config, icon = ds.pad(ds.icons.misc.Gear, "right"), desc = " Configuration File" },
-            { key = "l", action = ":Lazy", icon = ds.pad(ds.icons.misc.Sleep, "right"), desc = " Plugin Manager" },
+            { key = "l", action = ":Lazy", icon = ds.pad(ds.icons.misc.Extensions, "right"), desc = " Plugin Manager" },
             { key = "q", action = function() vim.api.nvim_input "<cmd>qa<cr>" end, icon = ds.pad(ds.icons.misc.Exit, "right"), desc = " Quit" },
           },
         },
