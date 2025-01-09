ed1280afc4f8c7c6892b573acd8d31c3ce0635d2
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-01-09 14:49:08 -0600

    fix(nvim): remove unecessary treesitter queries

--- nvim/queries/markdown/highlights.scm
+++ nvim/queries/markdown/highlights.scm
@@ -1,21 +1,5 @@
 ; extends
 
-(atx_h1_marker) @markup.heading
-((atx_h1_marker) heading_content: (_) @markup.heading)
-(atx_h2_marker) @markup.heading.sub
-((atx_h2_marker) heading_content: (_) @markup.heading.sub)
-
-[
-  (atx_h3_marker)
-  (atx_h4_marker)
-  (atx_h5_marker)
-  (atx_h6_marker)
-] @variable.builtin
-((atx_h3_marker) heading_content: (_) @variable.builtin)
-((atx_h4_marker) heading_content: (_) @variable.builtin)
-((atx_h5_marker) heading_content: (_) @variable.builtin)
-((atx_h6_marker) heading_content: (_) @variable.builtin)
-
 (list_item [
   (list_marker_plus)
   (list_marker_minus)
