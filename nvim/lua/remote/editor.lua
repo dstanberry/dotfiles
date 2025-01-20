1bc9a5f70e62b26d2b912a08c55bd5db7d167be6
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-01-20 10:41:47 -0600

    fix(nvim): improve `dial` configuration

--- nvim/lua/remote/editor.lua
+++ nvim/lua/remote/editor.lua
@@ -63,128 +63,66 @@ return {
   },
   {
     "monaqa/dial.nvim",
-    keys = {
-      { "<c-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "dial: increment" },
-      { "<c-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "dial: decrement" },
-    },
-    config = function()
+    keys = function()
+      local dial = function(increment)
+        local mode = vim.fn.mode(true)
+        -- Use visual commands for VISUAL 'v', VISUAL LINE 'V' and VISUAL BLOCK '\22'
+        local is_visual = mode == "v" or mode == "V" or mode == "\22"
+        local func = (increment and "inc_" or "dec_") .. (is_visual and "visual" or "normal")
+        local group = vim.g.dials_by_ft[vim.bo.filetype] or "default"
+        return require("dial.map")[func](group)
+      end
+      return {
+        { "<c-a>", function() return dial(true) end, expr = true, desc = "dial: increment", mode = { "n", "v" } },
+        { "<c-x>", function() return dial(false) end, expr = true, desc = "dial: decrement", mode = { "n", "v" } },
+      }
+    end,
+    opts = function()
       local augend = require "dial.augend"
-      local config = require "dial.config"
-
-      local ordinal_numbers = augend.constant.new {
-        elements = {
-          "first",
-          "second",
-          "third",
-          "fourth",
-          "fifth",
-          "sixth",
-          "seventh",
-          "eighth",
-          "ninth",
-          "tenth",
+      -- stylua: ignore
+      local ordinal_numbers = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth" }
+      -- stylua: ignore
+      local months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" }
+      local weekdays = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" }
+      return {
+        dials_by_ft = {
+          javascript = "typescript",
+          typescript = "typescript",
+          typescriptreact = "typescript",
+          javascriptreact = "typescript",
+          markdown = "markdown",
         },
-        word = false,
-        cyclic = true,
-      }
-
-      local weekdays = augend.constant.new {
-        elements = {
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-          "Sunday",
-        },
-        word = true,
-        cyclic = true,
-      }
-
-      local months = augend.constant.new {
-        elements = {
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December",
-        },
-        word = true,
-        cyclic = true,
-      }
-
-      config.augends:register_group {
-        default = {
-          augend.constant.alias.bool,
-          augend.integer.alias.decimal,
-          augend.integer.alias.hex,
-          augend.semver.alias.semver,
-          augend.date.alias["%Y/%m/%d"],
-          augend.constant.new {
-            elements = {
-              "True",
-              "False",
-            },
-            word = true,
-            cyclic = true,
+        groups = {
+          default = {
+            augend.constant.alias.bool,
+            augend.integer.alias.decimal,
+            augend.integer.alias.decimal_int,
+            augend.integer.alias.hex,
+            augend.semver.alias.semver,
+            augend.date.alias["%Y/%m/%d"],
+            augend.constant.new { elements = { "True", "False" }, word = true, cyclic = true },
+            augend.constant.new { elements = { "and", "or" }, word = true, cyclic = true },
+            augend.constant.new { elements = { "&&", "||" }, word = false, cyclic = true },
+            augend.constant.new { elements = ordinal_numbers, word = true, cyclic = true },
+            augend.constant.new { elements = weekdays, word = true, cyclic = true },
+            augend.constant.new { elements = months, word = true, cyclic = true },
           },
-          augend.constant.new {
-            elements = { "and", "or" },
-            word = true,
-            cyclic = true,
+          markdown = {
+            augend.misc.alias.markdown_header,
+            augend.constant.new { elements = { "[ ]", "[x]" }, word = false, cyclic = true },
           },
-          augend.constant.new {
-            elements = { "&&", "||" },
-            word = false,
-            cyclic = true,
+          typescript = {
+            augend.constant.new { elements = { "let", "const" }, word = true, cyclic = true },
           },
-          ordinal_numbers,
-          weekdays,
-          months,
-        },
-        markdown = {
-          augend.misc.alias.markdown_header,
-          augend.constant.alias.bool,
-          augend.integer.alias.decimal,
-          augend.integer.alias.hex,
-          augend.semver.alias.semver,
-          augend.date.alias["%Y/%m/%d"],
-          augend.constant.new {
-            elements = {
-              "True",
-              "False",
-            },
-            word = true,
-            cyclic = true,
-          },
-          augend.constant.new {
-            elements = { "and", "or" },
-            word = true,
-            cyclic = true,
-          },
-          augend.constant.new {
-            elements = { "&&", "||" },
-            word = false,
-            cyclic = true,
-          },
-          augend.constant.new {
-            elements = { "[ ]", "[x]" },
-            word = false,
-            cyclic = true,
-          },
-          ordinal_numbers,
-          weekdays,
-          months,
         },
       }
+    end,
+    config = function(_, opts)
+      for name, group in pairs(opts.groups) do
+        if name ~= "default" then vim.list_extend(group, opts.groups.default) end
+      end
+      require("dial.config").augends:register_group(opts.groups)
+      vim.g.dials_by_ft = opts.dials_by_ft
     end,
   },
   {
