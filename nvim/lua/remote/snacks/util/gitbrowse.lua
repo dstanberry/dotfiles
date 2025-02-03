aed4697e624645699e30a98edb1b91ba8c4bdc1a
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-02-03 13:00:50 -0600

    fix(nvim): `snacks.gitbrowse` config for github enterprise

--- nvim/lua/remote/snacks/util/gitbrowse.lua
+++ nvim/lua/remote/snacks/util/gitbrowse.lua
@@ -4,13 +4,19 @@ local M = {}
 local ghe = vim.g.ds_env.github_hostname
 
 M.browse = {
-  open = ds.has "wsl" and function(url) vim.system { "cmd.exe", "/c", "start", url } end,
-  url_patterns = ghe and { [ghe] = { branch = "/tree/{branch}", file = "/blob/{branch}/{file}#L{line}" } },
+  open = ds.has "wsl" and function(url) vim.system { "cmd.exe", "/c", "start", url } end or nil,
+  url_patterns = ghe and {
+    [ghe] = {
+      branch = "/tree/{branch}",
+      commit = "/commit/{commit}",
+      file = "/blob/{branch}/{file}#L{line_start}-L{line_end}",
+    },
+  } or {},
 }
 
 M.copy_url = {
   notify = false,
-  url_pattern = M.browse.url_patterns,
+  url_patterns = M.browse.url_patterns,
   open = function(url)
     vim.fn.setreg("+", url)
     ds.info("URL copied to clipboard.\n\n" .. url)
