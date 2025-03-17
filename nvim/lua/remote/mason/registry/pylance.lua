8fe56fc547bd9850ccbd8d34fcc8a7a54955d15e
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-03-17 10:03:18 -0500

    chore(nvim): improve variable names

--- nvim/lua/remote/mason/registry/pylance.lua
+++ nvim/lua/remote/mason/registry/pylance.lua
@@ -1,5 +1,4 @@
 local Pkg = require "mason-core.package"
-local path = require "mason-core.path"
 
 return Pkg.new {
   name = "pylance",
@@ -8,22 +7,22 @@ return Pkg.new {
   languages = { Pkg.Lang.Python },
   categories = { Pkg.Cat.LSP },
   install = function(ctx)
-    local dl = [[
+    local download_artifact = [[
       curl -s -c cookies.txt 'https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance' > /dev/null &&
       curl -s "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/vscode-pylance/latest/vspackage"
           -j -b cookies.txt --compressed --output "pylance.vsix"
     ]]
-    local edits = [[
+    local post_install_hook = [[
       perl -pe 's/if\(!process.*?\)return!\[\];/if(false)return false;/g; s/throw new//g' extension/dist/server.bundle.js
         > extension/dist/server.nvim.js
     ]]
     ctx.receipt:with_primary_source(ctx.receipt.unmanaged)
-    ctx.spawn.bash { "-c", dl:gsub("\n", " ") }
+    ctx.spawn.bash { "-c", download_artifact:gsub("\n", " ") }
     ctx.spawn.unzip { "pylance.vsix" }
-    ctx.spawn.bash { "-c", edits:gsub("\n", " ") }
+    ctx.spawn.bash { "-c", post_install_hook:gsub("\n", " ") }
     ctx:link_bin(
       "pylance",
-      ctx:write_node_exec_wrapper("pylance", path.concat { "extension", "dist", "server.nvim.js" })
+      ctx:write_node_exec_wrapper("pylance", vim.fs.joinpath("extension", "dist", "server.nvim.js"))
     )
   end,
 }
