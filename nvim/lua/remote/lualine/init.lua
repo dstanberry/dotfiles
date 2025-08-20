83edf1fd6a796be51ea7f512e7b90548733c27d2
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-08-20 16:49:57 -0500

    chore(nvim): semantic format

--- nvim/lua/remote/lualine/init.lua
+++ nvim/lua/remote/lualine/init.lua
@@ -3,13 +3,7 @@ return {
   event = "VeryLazy",
   init = function()
     vim.g.lualine_laststatus = vim.o.laststatus
-    if vim.fn.argc(-1) > 0 then
-      -- set an empty statusline till lualine loads
-      vim.o.statusline = " "
-    else
-      -- hide the statusline on the starter page
-      vim.o.laststatus = 0
-    end
+    vim.o.statusline = vim.fn.argc(-1) > 0 and " " or 0
   end,
   opts = function()
     local util = require "remote.lualine.util"
