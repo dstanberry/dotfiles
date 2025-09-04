0625d610833c8bf8f0196f13e588a0d20f3b814e
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-09-04 14:07:31 -0500

    fix(nvim): resolve `vim.lsp` issue with python

--- nvim/lsp/pylance.lua
+++ nvim/lsp/pylance.lua
@@ -4,12 +4,15 @@ local M = {}
 M.config = {
   cmd = { "pylance", "--stdio" },
   filetypes = { "python" },
-  root_dir = function(fname)
-    return ds.root.detectors.pattern(
-      fname,
-      { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile" }
-    )[1]
-  end,
+  root_markers = {
+    "pyproject.toml",
+    "setup.py",
+    "setup.cfg",
+    "requirements.txt",
+    "Pipfile",
+    "pyrightconfig.json",
+    ".git",
+  },
   single_file_support = true,
   settings = {
     python = {
