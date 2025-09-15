030b91421a42a9a4e1c19ed08ba69d70905baabd
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-09-15 15:18:36 -0500

    fix(nvim): lazy load `schemastore`

--- nvim/lsp/jsonls.lua
+++ nvim/lsp/jsonls.lua
@@ -2,10 +2,13 @@
 local M = {}
 
 M.config = {
+  before_init = function(_, new_config)
+    new_config.settings.json.schemas =
+      vim.tbl_deep_extend("force", new_config.settings.json.schemas or {}, require("schemastore").json.schemas())
+  end,
   init_options = { provideFormatter = false },
   settings = {
     json = {
-      schemas = require("schemastore").json.schemas(),
       validate = { enable = true },
     },
   },
--- nvim/lsp/yamlls.lua
+++ nvim/lsp/yamlls.lua
@@ -2,6 +2,10 @@
 local M = {}
 
 M.config = {
+  before_init = function(_, new_config)
+    new_config.settings.json.schemas =
+      vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
+  end,
   on_attach = function(_, bufnr)
     if vim.bo[bufnr].filetype == "helm" then vim.schedule(function() vim.cmd "LspStop ++force yamlls" end) end
   end,
@@ -15,7 +19,6 @@ M.config = {
     yaml = {
       keyOrdering = false,
       format = { enable = true },
-      schemas = require("schemastore").yaml.schemas(),
       schemaStore = { enable = false, url = "" },
       validate = true,
     },
