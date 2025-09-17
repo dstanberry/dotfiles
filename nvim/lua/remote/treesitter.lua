00b4208b3e4c214898ef8de4b233b2117f9274fc
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-09-17 17:09:59 -0500

    chore(nvim): add autocmd group name

--- nvim/lua/remote/treesitter.lua
+++ nvim/lua/remote/treesitter.lua
@@ -46,6 +46,7 @@ return {
       local indent = vim.tbl_get(opts, "indent", "enable")
       if highlight or indent then
         vim.api.nvim_create_autocmd("FileType", {
+          group = ds.augroup "treesitter",
           callback = function(event)
             local lang = vim.treesitter.language.get_lang(event.match)
             if not vim.tbl_contains(installed, lang) then return end
