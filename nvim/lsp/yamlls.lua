cd423c848f8e29077b559d193349f56f2e403241
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-10-01 11:46:24 -0500

    fix(nvim): yaml lsp config

--- nvim/lsp/yamlls.lua
+++ nvim/lsp/yamlls.lua
@@ -3,7 +3,7 @@ local M = {}
 
 M.config = {
   before_init = function(_, new_config)
-    new_config.settings.json.schemas =
+    new_config.settings.yaml.schemas =
       vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
   end,
   on_attach = function(_, bufnr)
