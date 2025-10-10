035eaf760a6deb997746225fc03d9da3ab8f8114
Demaro Stanberry <demaro.stanberry@gmail.com>
2025-10-09 20:30:18 -0500

    fix(nvim): `prettier` formatting specs

--- nvim/lua/remote/editor.lua
+++ nvim/lua/remote/editor.lua
@@ -200,6 +200,7 @@ return {
           prettier = {
             prepend_args = function(_, ctx)
               local config = get_config("prettier", ctx)
+              config = vim.uv.fs_realpath(config) or nil
               local fallback = vim.fs.joinpath(vim.env.XDG_CONFIG_HOME, "shared", "formatters", "prettierrc.json")
               return { "--config", config or fallback }
             end,
